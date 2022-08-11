e9441f00879ba0ed5896388242be096ccee87aeb
juanisantamarina <107272429+juanisantamarina@users.noreply.github.com>
2022-08-11 16:22:06 -0300

    Initial commit

---
+++ src/main/java/com/alkemy/ong/OngApplication.java
@@ -0,0 +1,13 @@
+package com.alkemy.ong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OngApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alkemy/ong/OngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alkemy.ong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
