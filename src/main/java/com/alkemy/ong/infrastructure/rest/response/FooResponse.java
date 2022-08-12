4bf836e43f84aca40ad839b7837b33bdab6d032c
Magali Kain <magali.kain@thoughtworks.com>
2022-08-12 13:52:12 +0200

    Create expected response for API

---
+++ src/main/java/com/alkemy/ong/infrastructure/rest/response/FooResponse.java
@@ -0,0 +1,14 @@
+package com.alkemy.ong.infrastructure.rest.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class FooResponse {
+
+  private String name;
+
+}
---
+++ src/main/java/com/alkemy/ong/infrastructure/rest/response/ListFooResponse.java
@@ -0,0 +1,15 @@
+package com.alkemy.ong.infrastructure.rest.response;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class ListFooResponse {
+
+  private List<FooResponse> foos;
+
+}
