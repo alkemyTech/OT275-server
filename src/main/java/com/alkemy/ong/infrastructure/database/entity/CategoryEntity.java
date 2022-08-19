35079700b8fa8a23aff15a20e91f8e30a1907488
Ezequiel Villafañe <eze.villafae07@gmail.com>
2022-08-19 09:17:23 -0300

    OT275-19: Added category entity

---
+++ src/main/java/com/alkemy/ong/infrastructure/database/entity/CategoryEntity.java
@@ -0,0 +1,49 @@
+package com.alkemy.ong.infrastructure.database.entity;
+
+import java.sql.Timestamp;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@SQLDelete(sql = "UPDATE CATEGORIES SET SOFT_DELETED = true WHERE CATEGORY_ID=?")
+@Where(clause = "SOFT_DELETED = false")
+@Entity
+@Table(name = "CATEGORIES")
+public class CategoryEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "CATEGORY_ID")
+  private Long categoryId;
+
+  @Column(name = "NAME", nullable = false)
+  private String name;
+
+  @Column(name = "DESCRIPTION")
+  private String description;
+
+  @Column(name = "IMAGE_URL")
+  private String imageUrl;
+
+  @Column(name = "CREATE_TIMESTAMP", updatable = false)
+  @CreationTimestamp
+  private Timestamp createTimestamp;
+
+  @Column(name = "SOFT_DELETED")
+  private boolean softDeleted;
+
+}
