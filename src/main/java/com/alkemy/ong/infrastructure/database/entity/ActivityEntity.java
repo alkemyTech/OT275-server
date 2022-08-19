a308b2c2e78e337979d8cdf291e32f90515fcede
rene-guardia <reneguardia@outlook.com>
2022-08-19 12:01:29 -0300

    OT275-22: Added Activity entity

---
+++ src/main/java/com/alkemy/ong/infrastructure/database/entity/ActivityEntity.java
@@ -0,0 +1,49 @@
+package com.alkemy.ong.infrastructure.database.entity;
+
+import java.sql.Timestamp;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "ACTIVITIES")
+@SQLDelete(sql = "UPDATE ACTIVITIES SET SOFT_DELETED = true WHERE ACTIVITY_ID = ?")
+@Where(clause = "SOFT_DELETED = false")
+public class ActivityEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "ACTIVITY_ID")
+  private Long activityId;
+
+  @Column(name = "NAME", nullable = false)
+  private String name;
+
+  @Column(name = "CONTENT", nullable = false)
+  private String content;
+
+  @Column(name = "IMAGE_URL", nullable = false)
+  private String imageUrl;
+
+  @CreationTimestamp
+  @Column(name = "CREATE_TIMESTAMP", updatable = false)
+  private Timestamp createTimestamp;
+
+  @Column(name = "SOFT_DELETED")
+  private boolean softDeleted;
+
+}
