b83c90fa53abd9a9636b4ca004d36c9c21e4d1a3
Ezequiel Villafañe <eze.villafae07@gmail.com>
2022-08-19 14:25:32 -0300

    OT275-19: fix soft delete query removed from category entity

--- src/main/java/com/alkemy/ong/infrastructure/database/entity/CategoryEntity.java
+++ src/main/java/com/alkemy/ong/infrastructure/database/entity/CategoryEntity.java
@@ -12,15 +12,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.SQLDelete;
-import org.hibernate.annotations.Where;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@SQLDelete(sql = "UPDATE CATEGORIES SET SOFT_DELETED = true WHERE CATEGORY_ID=?")
-@Where(clause = "SOFT_DELETED = false")
 @Entity
 @Table(name = "CATEGORIES")
 public class CategoryEntity {
