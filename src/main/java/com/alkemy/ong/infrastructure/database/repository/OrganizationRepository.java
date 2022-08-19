a694fdc663cf1f6971de170745202f221d7d43ed
FedericoRohr <rohrfede@gmail.com>
2022-08-19 15:20:58 -0300

    OT275-16 :infrastructure/database fixed OrganizationRepostory

--- src/main/java/com/alkemy/ong/infrastructure/database/repository/OrganizationRepository.java
+++ src/main/java/com/alkemy/ong/infrastructure/database/repository/OrganizationRepository.java
@@ -2,9 +2,9 @@ package com.alkemy.ong.infrastructure.database.repository;
 
 import org.springframework.stereotype.Component;
 import com.alkemy.ong.application.repository.IOrganizationRepository;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Component
 public class OrganizationRepository implements IOrganizationRepository {
 
