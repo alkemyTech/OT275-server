795fbc312b18d5e8c13b3c022d5a2490e40d99ba
rene-guardia <reneguardia@outlook.com>
2022-08-19 14:43:58 -0300

    OT275-22: Removed unused imports

--- src/main/java/com/alkemy/ong/infrastructure/database/entity/ActivityEntity.java
+++ src/main/java/com/alkemy/ong/infrastructure/database/entity/ActivityEntity.java
@@ -12,8 +12,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.SQLDelete;
-import org.hibernate.annotations.Where;
 
 @Getter
 @Setter
