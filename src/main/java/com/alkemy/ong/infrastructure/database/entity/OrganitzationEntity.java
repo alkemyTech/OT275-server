165414b2f0c369221716dc6ce66fcc9ef4575a92
FedericoRohr <rohrfede@gmail.com>
2022-08-19 15:09:07 -0300

    OT275-16 : add database organization entity

---
+++ src/main/java/com/alkemy/ong/infrastructure/database/entity/OrganitzationEntity.java
@@ -0,0 +1,60 @@
+package com.alkemy.ong.infrastructure.database.entity;
+import static javax.persistence.GenerationType.IDENTITY;
+import java.sql.Timestamp;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import org.hibernate.annotations.CreationTimestamp;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@Getter
+@Setter
+@Table(name= "organizations")
+public class OrganitzationEntity {
+ 
+  @Id
+  @GeneratedValue(strategy = IDENTITY)
+  @Column(name = "ORGANIZATION_ID")
+  private Long organizationId;
+  
+  @Column (name = "NAME" , nullable = false)
+  private String name;
+  
+  @Column (name = "IMAGE_URL" , nullable = false)
+  private String imageUrl;
+  
+  @Column (name = "ADDRESS")
+  private String address;
+  
+  @Column (name = "PHONE")
+  private String phone;
+  
+  @Column (name = "EMAIL", nullable = false)
+  private String email;
+  
+  @Column (name = "WELCOME_TEXT", nullable = false)
+  private String welcomeText;
+  
+  @Column (name = "ABOUT_US", nullable = false)
+  private String aboutUsText;
+  
+  @Column(name = "CREATE_TIMESTAMP", updatable = false)
+  @CreationTimestamp
+  private Timestamp createTimestamp;
+  
+  @Column(name = "SOFT_DELETED", updatable = false)
+  private boolean softDelete;
+  
+  
+  
+  
+  
+
+  
+}
