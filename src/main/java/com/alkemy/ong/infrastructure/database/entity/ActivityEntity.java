f6d501a8cc1c4cfe7363be3a2c2cca10e3eb9d08
guardrn <108477465+guardrn@users.noreply.github.com>
2022-08-19 16:07:23 -0300

    OT275-22: Create model and repository of Activities (#4)

---
+++ src/main/java/com/alkemy/ong/infrastructure/database/entity/ActivityEntity.java
@@ -0,0 +1,45 @@
+package com.alkemy.ong.infrastructure.database.entity;
+
+import java.sql.Timestamp;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "ACTIVITIES")
+public class ActivityEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "ACTIVITY_ID")
+  private Long activityId;
+
+  @Column(name = "NAME", nullable = false)
+  private String name;
+
+  @Column(name = "CONTENT", nullable = false)
+  private String content;
+
+  @Column(name = "IMAGE_URL", nullable = false)
+  private String imageUrl;
+
+  @CreationTimestamp
+  @Column(name = "CREATE_TIMESTAMP", updatable = false)
+  private Timestamp createTimestamp;
+
+  @Column(name = "SOFT_DELETED")
+  private boolean softDeleted;
+
+}
