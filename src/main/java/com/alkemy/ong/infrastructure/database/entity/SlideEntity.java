d84ff08dfda62497807aa33bb83502473d325423
Joel Luján <86501243+Joelzlujan@users.noreply.github.com>
2022-08-20 14:56:41 -0300

    OT275-29: Create entity and repository of Slide (#12)

---
+++ src/main/java/com/alkemy/ong/infrastructure/database/entity/SlideEntity.java
@@ -0,0 +1,35 @@
+package com.alkemy.ong.infrastructure.database.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "SLIDES")
+public class SlideEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "SLIDE_ID")
+  private Long slideId;
+
+  @Column(name = "IMAGE_URL", nullable = false)
+  private String imageUrl;
+
+  @Column(name = "TEXT")
+  private String text;
+
+  @Column(name = "POSITION", nullable = false)
+  private Integer position;
+}
---
+++ src/main/java/com/alkemy/ong/infrastructure/database/repository/ISlideSpringRepository.java
@@ -0,0 +1,10 @@
+package com.alkemy.ong.infrastructure.database.repository;
+
+import com.alkemy.ong.infrastructure.database.entity.SlideEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ISlideSpringRepository extends JpaRepository<SlideEntity, Long> {
+
+}
