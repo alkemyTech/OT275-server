646cf7194c5e6e9117acde982ce41a34072e91da
Cristian David Moraleda <73443385+Sieglind@users.noreply.github.com>
2022-08-20 17:13:16 -0300

    OT275-32: Create JwtUtils and request filter (#9)

---
+++ src/main/java/com/alkemy/ong/infrastructure/config/spring/security/common/JwtUtils.java
@@ -0,0 +1,83 @@
+package com.alkemy.ong.infrastructure.config.spring.security.common;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import java.time.ZonedDateTime;
+import java.util.Base64;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class JwtUtils {
+
+  private static final String SECRET_KEY = Base64.getEncoder().encodeToString("secret".getBytes());
+  private static final String BEARER_PART = "Bearer ";
+  private static final String EMPTY = "";
+  private static final String ROLES_CLAIM = "roles";
+
+  private JwtUtils() {
+  }
+
+  public static String create(UserDetails userDetails) {
+    return Jwts.builder()
+        .setSubject(userDetails.getUsername())
+        .claim(ROLES_CLAIM, userDetails.getAuthorities().stream()
+            .map(GrantedAuthority::getAuthority)
+            .collect(Collectors.toList()))
+        .setIssuedAt(Date.from(ZonedDateTime.now().toInstant()))
+        .setExpiration(Date.from(ZonedDateTime.now().plusMinutes(90).toInstant()))
+        .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+        .compact();
+  }
+
+  public static Jwt extract(String authorizationHeader) {
+    return Jwt.extract(authorizationHeader);
+  }
+
+  public static class Jwt {
+
+    private final String username;
+
+    private final List<GrantedAuthority> grantedAuthorities;
+
+    private Jwt(String token) {
+      Claims claims = extractAllClaims(token);
+      this.username = claims.getSubject();
+      this.grantedAuthorities = buildGrantedAuthorities(claims);
+    }
+
+    public static Jwt extract(String authorizationHeader) {
+      return new Jwt(getTokenFrom(authorizationHeader));
+    }
+
+    public String getUsername() {
+      return username;
+    }
+
+    public List<GrantedAuthority> getGrantedAuthorities() {
+      return grantedAuthorities;
+    }
+
+    private List<GrantedAuthority> buildGrantedAuthorities(Claims claims) {
+      return AuthorityUtils.commaSeparatedStringToAuthorityList(
+          Objects.toString(claims.get(ROLES_CLAIM)));
+    }
+
+    private static String getTokenFrom(String authorizationHeader) {
+      return authorizationHeader.replace(BEARER_PART, EMPTY);
+    }
+
+    private static Claims extractAllClaims(String token) {
+      return Jwts.parser()
+          .setSigningKey(SECRET_KEY)
+          .parseClaimsJws(token)
+          .getBody();
+    }
+
+  }
+}
---
+++ src/main/java/com/alkemy/ong/infrastructure/config/spring/security/filter/AuthorizationFilter.java
@@ -0,0 +1,53 @@
+package com.alkemy.ong.infrastructure.config.spring.security.filter;
+
+import com.alkemy.ong.infrastructure.config.spring.security.common.JwtUtils;
+import com.alkemy.ong.infrastructure.config.spring.security.common.JwtUtils.Jwt;
+import io.jsonwebtoken.JwtException;
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+public class AuthorizationFilter extends OncePerRequestFilter {
+
+  private static final String BEARER_PART = "Bearer ";
+  private static final Object CREDENTIALS = null;
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+      FilterChain filterChain) throws ServletException, IOException {
+    String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+    if (!isValid(authorizationHeader)) {
+      SecurityContextHolder.clearContext();
+      filterChain.doFilter(request, response);
+      return;
+    }
+
+    try {
+      Jwt jwt = JwtUtils.extract(authorizationHeader);
+      Authentication authentication = new UsernamePasswordAuthenticationToken(
+          jwt.getUsername(),
+          CREDENTIALS,
+          jwt.getGrantedAuthorities()
+      );
+      SecurityContextHolder.getContext().setAuthentication(authentication);
+    } catch (JwtException e) {
+      throw new IllegalStateException("Invalid JWT signature.");
+    }
+  }
+
+  private boolean isValid(String authorizationHeader) {
+    return StringUtils.hasText(authorizationHeader) && authorizationHeader.startsWith(BEARER_PART);
+  }
+
+}
