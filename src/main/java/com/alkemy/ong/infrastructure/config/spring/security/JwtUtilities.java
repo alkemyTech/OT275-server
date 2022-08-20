1c8fb98b08d36cfd8213a6e10a7c103259077d92
Cristian David Moraleda <moraledacd@gmail.com>
2022-08-20 00:06:01 -0300

    OT275-32: Added "getUsername" and "getRoles" methods to "JwtUtilities"

--- src/main/java/com/alkemy/ong/infrastructure/config/spring/security/JwtUtilities.java
+++ src/main/java/com/alkemy/ong/infrastructure/config/spring/security/JwtUtilities.java
@@ -1,15 +1,20 @@
 package com.alkemy.ong.infrastructure.config.spring.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import java.sql.Date;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
+import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
 
-public class jwtUtilities {
+@Component
+public class JwtUtilities {
 
   public String createJwToken(Authentication authenticationResult) {
     return Jwts.builder()
@@ -20,5 +25,19 @@ public class jwtUtilities {
         .setExpiration(Date.valueOf(LocalDate.now().plus(30, ChronoUnit.MINUTES)))
         .signWith(SignatureAlgorithm.HS256, "secret")
         .compact();
+  }
+
+  public Claims getClaims(String jwToken) throws JwtException {
+    return Jwts.parser()
+          .setSigningKey("secret")
+          .parseClaimsJws(jwToken).getBody();
+  }
+
+  public String getUsername(String jwToken) {
+    return getClaims(jwToken).getSubject();
+  }
+
+  public List<String> getRoles(String jwToken) {
+    return (List<String>) getClaims(jwToken).get("roles");
   }
 }
