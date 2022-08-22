f175e257cc32ed1841375a11f1ac2f0fcc4581ef
Joel Luján <86501243+Joelzlujan@users.noreply.github.com>
2022-08-22 13:22:53 -0300

    OT275-75: Create entity and repository of Comment (#15)

---
+++ src/main/java/com/alkemy/ong/infrastructure/database/entity/CommentEntity.java
@@ -0,0 +1,45 @@
+package com.alkemy.ong.infrastructure.database.entity;
+
+import java.sql.Timestamp;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "COMMENTS")
+public class CommentEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "COMMENT_ID")
+  private Long commentId;
+
+  @Column(name = "CREATE_TIMESTAMP", updatable = false)
+  @CreationTimestamp
+  private Timestamp createTimestamp;
+
+  @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.PERSIST}, fetch = FetchType.LAZY)
+  @JoinColumn(name = "USER_ID", insertable = false, updatable = false)
+  private UserEntity user;
+
+  @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.PERSIST}, fetch = FetchType.LAZY)
+  @JoinColumn(name = "NEWS_ID", insertable = false, updatable = false)
+  private NewsEntity news;
+
+}
