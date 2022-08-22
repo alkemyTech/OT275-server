768d1619526c558ee3ed6dcf5261349014c7c2ad
FedericoRohr <rohrfede@gmail.com>
2022-08-22 15:19:24 -0300

    OT275-74 :remove csrf

--- src/main/java/com/alkemy/ong/infrastructure/config/spring/security/SecurityConfig.java
+++ src/main/java/com/alkemy/ong/infrastructure/config/spring/security/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.csrf().disable().authorizeRequests().anyRequest().authenticated().and().sessionManagement()
+    http.authorizeRequests().anyRequest().authenticated().and().sessionManagement()
         .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
     http.addFilterBefore(authorizationFilter, UsernamePasswordAuthenticationFilter.class);
