6c3612ca34e6a18c0b6168498ff246cdb5f5e156
FedericoRohr <rohrfede@gmail.com>
2022-08-22 14:30:46 -0300

    OT275-74 : added filter and auth to any request in configure method

--- src/main/java/com/alkemy/ong/infrastructure/config/spring/security/SecurityConfig.java
+++ src/main/java/com/alkemy/ong/infrastructure/config/spring/security/SecurityConfig.java
@@ -1,20 +1,30 @@
 package com.alkemy.ong.infrastructure.config.spring.security;
 
+import com.alkemy.ong.infrastructure.config.spring.security.filter.AuthorizationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+  @Autowired
+  private AuthorizationFilter authorizationFilter;
+
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.authorizeRequests().anyRequest().permitAll();
+    http.csrf().disable().authorizeRequests().anyRequest().authenticated().and().sessionManagement()
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+    http.addFilterBefore(authorizationFilter, UsernamePasswordAuthenticationFilter.class);
   }
 
 }
