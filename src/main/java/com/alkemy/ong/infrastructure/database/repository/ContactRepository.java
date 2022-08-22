b9ea48708f4e0f3693f39e8485235c0deb54e4c0
Liliana <68357688+LAYALA09@users.noreply.github.com>
2022-08-22 12:27:10 -0300

    OT275-54: Create entity and repository of Contacts (#13)

---
+++ src/main/java/com/alkemy/ong/infrastructure/database/repository/ContactRepository.java
@@ -0,0 +1,13 @@
+package com.alkemy.ong.infrastructure.database.repository;
+
+import com.alkemy.ong.application.repository.IContactRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class ContactRepository implements IContactRepository {
+
+  private final IContactSpringRepository contactSpringRepository;
+
+}
