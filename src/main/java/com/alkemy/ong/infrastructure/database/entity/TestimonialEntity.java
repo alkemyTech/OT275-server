7fc1ec5bccc54b03ecd5bf5e6472762b57f22d19
Liliana <68357688+LAYALA09@users.noreply.github.com>
2022-08-22 12:25:54 -0300

    OT275-21: Create entity and repository of Testimonials (#14)

---
+++ src/main/java/com/alkemy/ong/infrastructure/database/entity/TestimonialEntity.java
@@ -0,0 +1,47 @@
+package com.alkemy.ong.infrastructure.database.entity;
+
+import java.sql.Timestamp;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table(name = "TESTIMONIALS")
+public class TestimonialEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "TESTIMONIAL_ID")
+  private Long testimonialId;
+
+  @Column(name = "NAME", nullable = false)
+  private String name;
+
+  @Column(name = "IMAGE_URL")
+  private String imageUrl;
+
+  @Column(name = "CONTENT")
+  private String content;
+
+  @Column(name = "CREATE_TIMESTAMP", updatable = false)
+  @CreationTimestamp
+  private Timestamp createTimestamp;
+
+  @Column(name = "SOFT_DELETED")
+  private boolean softDeleted;
+
+}
+
+
