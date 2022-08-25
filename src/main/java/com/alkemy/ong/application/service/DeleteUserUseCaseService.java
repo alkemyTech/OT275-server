ca303d635a97d6ef9cf599b12452050e810b7f06
Joel Lujan <joelandreslujan@gmail.com>
2022-08-25 13:06:03 -0300

    OT275-33: Implements delete method to delete use case service

--- src/main/java/com/alkemy/ong/application/service/DeleteUserUseCaseService.java
+++ src/main/java/com/alkemy/ong/application/service/DeleteUserUseCaseService.java
@@ -4,14 +4,17 @@ import com.alkemy.ong.application.repository.IUserRepository;
 import com.alkemy.ong.application.service.usecase.IDeleteUserUseCase;
 import com.alkemy.ong.domain.Identifiable;
 import lombok.AllArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
 
 @AllArgsConstructor
 public class DeleteUserUseCaseService implements IDeleteUserUseCase {
 
   private final IUserRepository userRepository;
 
+  @Transactional
   @Override
   public void delete(Identifiable<Long> identifiable) {
 
+    userRepository.delete(identifiable);
   }
 }
