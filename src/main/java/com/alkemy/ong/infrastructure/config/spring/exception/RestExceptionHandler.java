c13af852b3de9ac33a9ba35be9fca8d4f370c963
Magali Kain <magali.kain@thoughtworks.com>
2022-08-26 17:55:30 +0200

    Add generic exception to RestExceptionHandler

--- src/main/java/com/alkemy/ong/infrastructure/config/spring/exception/RestExceptionHandler.java
+++ src/main/java/com/alkemy/ong/infrastructure/config/spring/exception/RestExceptionHandler.java
@@ -16,28 +16,40 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class RestExceptionHandler {
 
   private static final String OBJECT_NOT_FOUND = "Object not found in database.";
-  private static final String INVALID_ARGUMENT = "Invalid input data";
+  private static final String INVALID_ARGUMENT = "Invalid input data.";
+  private static final String ERROR_OCCURS = "Application has encountered an error.";
+
+  @ExceptionHandler(value = Exception.class)
+  protected ResponseEntity<ErrorResponse> handleGenericException(Exception e) {
+    ErrorResponse errorResponse = buildErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR,
+        ERROR_OCCURS,
+        e);
+
+    return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
 
   @ExceptionHandler(value = ObjectNotFound.class)
   protected ResponseEntity<ErrorResponse> handleObjectNotFound(ObjectNotFound e) {
-    ErrorResponse errorResponse = new ErrorResponse(HttpStatus.NOT_FOUND.value(), OBJECT_NOT_FOUND,
-        e.getMessage());
+    ErrorResponse errorResponse = buildErrorResponse(HttpStatus.NOT_FOUND, OBJECT_NOT_FOUND, e);
+
     return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
   }
 
   @ExceptionHandler(value = MethodArgumentNotValidException.class)
   protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValid(
-      MethodArgumentNotValidException ex) {
-    List<String> errors = collectErrors(ex);
-
-    ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(),
-        INVALID_ARGUMENT, errors);
+      MethodArgumentNotValidException e) {
+    ErrorResponse errorResponse = buildErrorResponse(HttpStatus.BAD_REQUEST,
+        INVALID_ARGUMENT,
+        collectErrors(e));
 
     return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
   }
 
   private List<String> collectErrors(MethodArgumentNotValidException ex) {
-    return ex.getBindingResult().getFieldErrors().stream().map(this::formatErrorField)
+    return ex.getBindingResult()
+        .getFieldErrors()
+        .stream()
+        .map(this::formatErrorField)
         .collect(Collectors.toList());
   }
 
@@ -45,4 +57,13 @@ public class RestExceptionHandler {
     return String.format("%s : %s", fieldError.getField(), fieldError.getDefaultMessage());
   }
 
+  private static ErrorResponse buildErrorResponse(HttpStatus httpStatus, String message,
+      Exception e) {
+    return new ErrorResponse(httpStatus.value(), message, e.getMessage());
+  }
+
+  private static ErrorResponse buildErrorResponse(HttpStatus httpStatus, String message,
+      List<String> moreInfo) {
+    return new ErrorResponse(httpStatus.value(), message, moreInfo);
+  }
 }
