6bd0ed69f2b9e77cd5bfd01752a3c9b602678a11
Magali Kain <magali.kain@thoughtworks.com>
2022-08-27 01:40:02 +0200

    Add example of unit test using jUnit5

---
+++ src/test/java/com/alkemy/ong/application/service/DeleteUserUseCaseServiceTest.java
@@ -0,0 +1,49 @@
+package com.alkemy.ong.application.service;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+import com.alkemy.ong.application.exception.ObjectNotFound;
+import com.alkemy.ong.application.repository.IUserRepository;
+import com.alkemy.ong.domain.Identifiable;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class DeleteUserUseCaseServiceTest {
+
+  private DeleteUserUseCaseService userService;
+
+  @Mock
+  private IUserRepository userRepository;
+
+  @Mock
+  private Identifiable<Long> identifiable;
+
+  @BeforeEach
+  void setup() {
+    userService = new DeleteUserUseCaseService(userRepository);
+  }
+
+  @Test
+  void shouldThrowExceptionWhenUserDoesNotExist() {
+    given(userRepository.exists(identifiable)).willReturn(false);
+
+    assertThrows(ObjectNotFound.class, () -> userService.delete(identifiable));
+  }
+
+  @Test
+  void shouldDeleteUserWhenUserExist() {
+    given(userRepository.exists(identifiable)).willReturn(true);
+
+    userService.delete(identifiable);
+
+    verify(userRepository).exists(identifiable);
+    verify(userRepository).delete(identifiable);
+  }
+
+}
