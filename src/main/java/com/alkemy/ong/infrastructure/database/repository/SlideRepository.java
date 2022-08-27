d50cf6732eb9a40b581f62385313062f34151637
Ricardo Olivera <96498642+r1ckt@users.noreply.github.com>
2022-08-27 07:51:32 -0300

    OT275-60: delete slide endpoint (#20)

--- src/main/java/com/alkemy/ong/infrastructure/database/repository/SlideRepository.java
+++ src/main/java/com/alkemy/ong/infrastructure/database/repository/SlideRepository.java
@@ -1,6 +1,7 @@
 package com.alkemy.ong.infrastructure.database.repository;
 
 import com.alkemy.ong.application.repository.ISlideRepository;
+import com.alkemy.ong.domain.Identifiable;
 import com.alkemy.ong.infrastructure.database.repository.abstraction.ISlideSpringRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -10,4 +11,14 @@ import org.springframework.stereotype.Component;
 public class SlideRepository implements ISlideRepository {
 
   private final ISlideSpringRepository slideSpringRepository;
+
+  @Override
+  public boolean exists(Identifiable<Long> identifiable) {
+    return slideSpringRepository.existsById(identifiable.getId());
+  }
+
+  @Override
+  public void delete(Identifiable<Long> identifiable) {
+    slideSpringRepository.deleteById(identifiable.getId());
+  }
 }
