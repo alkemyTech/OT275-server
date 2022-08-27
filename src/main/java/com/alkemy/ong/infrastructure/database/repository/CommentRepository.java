4550094f27998e235453afb388ac6ca17d6b7b0b
Ezequiel Villafañe <eze.villafae07@gmail.com>
2022-08-27 18:11:21 -0300

    OT275-79: Added find operation comment repository

--- src/main/java/com/alkemy/ong/infrastructure/database/repository/CommentRepository.java
+++ src/main/java/com/alkemy/ong/infrastructure/database/repository/CommentRepository.java
@@ -1,8 +1,11 @@
 package com.alkemy.ong.infrastructure.database.repository;
 
 import com.alkemy.ong.application.repository.ICommentRepository;
+import com.alkemy.ong.domain.Comment;
 import com.alkemy.ong.domain.Identifiable;
 import com.alkemy.ong.infrastructure.database.repository.abstraction.ICommentSpringRepository;
+import com.alkemy.ong.infrastructure.rest.mapper.ICommentMapper;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -12,6 +15,8 @@ public class CommentRepository implements ICommentRepository {
 
   private final ICommentSpringRepository commentSpringRepository;
 
+  private final ICommentMapper commentMapper;
+
   @Override
   public void delete(Identifiable<Long> identifiable) {
     commentSpringRepository.deleteById(identifiable.getId());
@@ -20,5 +25,10 @@ public class CommentRepository implements ICommentRepository {
   @Override
   public boolean exists(Identifiable<Long> identifiable) {
     return commentSpringRepository.existsById(identifiable.getId());
+  }
+
+  @Override
+  public Optional<Comment> find(Identifiable<Long> identifiable) {
+    return commentSpringRepository.findById(identifiable.getId()).map(commentMapper::toDomain);
   }
 }
