29ace4e98b57af7a68c33b4a7d77eacd0307ef80
Magali Kain <magali.kain@thoughtworks.com>
2022-08-27 23:05:22 +0200

    Remove unnecesary access modifier from test package

--- src/test/java/com/alkemy/ong/application/service/DeleteTestimonialUseCaseServiceTest.java
+++ src/test/java/com/alkemy/ong/application/service/DeleteTestimonialUseCaseServiceTest.java
@@ -14,7 +14,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
-public class DeleteTestimonialUseCaseServiceTest {
+class DeleteTestimonialUseCaseServiceTest {
 
   private DeleteTestimonialUseCaseService testimonialService;
 
