48dfad6aebf391e5ac1bdf25bdf3253c47fe1e76
Ezequiel Villafañe <eze.villafae07@gmail.com>
2022-08-27 16:37:18 -0300

    OT275-79: Configure dependencies of DeleteCommentUseCaseService

--- src/main/java/com/alkemy/ong/infrastructure/config/spring/SpringBeanConfiguration.java
+++ src/main/java/com/alkemy/ong/infrastructure/config/spring/SpringBeanConfiguration.java
@@ -1,9 +1,13 @@
 package com.alkemy.ong.infrastructure.config.spring;
 
+import com.alkemy.ong.application.repository.ICommentRepository;
 import com.alkemy.ong.application.repository.ISlideRepository;
 import com.alkemy.ong.application.repository.IUserRepository;
+import com.alkemy.ong.application.service.DeleteCommentUseCaseService;
 import com.alkemy.ong.application.service.DeleteSlideUseCaseService;
 import com.alkemy.ong.application.service.DeleteUserUseCaseService;
+import com.alkemy.ong.application.service.usecase.IAuthorization;
+import com.alkemy.ong.application.service.usecase.IDeleteCommentUseCase;
 import com.alkemy.ong.application.service.usecase.IDeleteSlideUseCase;
 import com.alkemy.ong.application.service.usecase.IDeleteUserUseCase;
 import org.springframework.context.annotation.Bean;
@@ -21,4 +25,11 @@ public class SpringBeanConfiguration {
   public IDeleteSlideUseCase deleteSlideUseCase(ISlideRepository slideRepository) {
     return new DeleteSlideUseCaseService(slideRepository);
   }
+
+  @Bean
+  public IDeleteCommentUseCase deleteCommentUseCase(ICommentRepository commentRepository,
+      IAuthorization authorization) {
+    return new DeleteCommentUseCaseService(commentRepository, authorization);
+  }
+
 }
