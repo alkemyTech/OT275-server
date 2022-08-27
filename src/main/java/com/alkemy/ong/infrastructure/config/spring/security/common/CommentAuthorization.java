4c5e4f9a6f84eaeb87764d3fafd90fc547699d2d
Ezequiel Villafañe <eze.villafae07@gmail.com>
2022-08-27 18:31:15 -0300

    OT275-79: Implements authorization logic

--- src/main/java/com/alkemy/ong/infrastructure/config/spring/security/common/CommentAuthorization.java
+++ src/main/java/com/alkemy/ong/infrastructure/config/spring/security/common/CommentAuthorization.java
@@ -1,13 +1,49 @@
 package com.alkemy.ong.infrastructure.config.spring.security.common;
 
+import com.alkemy.ong.application.repository.ICommentRepository;
 import com.alkemy.ong.application.service.usecase.IAuthorization;
+import com.alkemy.ong.domain.Comment;
 import com.alkemy.ong.domain.Identifiable;
+import com.alkemy.ong.infrastructure.rest.mapper.ICommentMapper;
+import com.alkemy.ong.infrastructure.rest.response.CommentResponse;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 
+@Component
+@RequiredArgsConstructor
 public class CommentAuthorization implements IAuthorization {
+
+  private final ICommentRepository commentRepository;
+  private final ICommentMapper commentMapper;
 
   @Override
   public boolean isAuthorized(Identifiable<Long> identifiable) {
-    return false;
+    Optional<Comment> comment = commentRepository.find(identifiable);
+    Optional<CommentResponse> commentResponse = comment.map(commentMapper::fromDomain);
+
+    Authentication authentication = getAuthentication();
+
+    return commentResponse.filter(
+        response -> isAdmin(authentication) || isUserCreator(authentication, response)).isPresent();
+  }
+
+  private Authentication getAuthentication() {
+    return SecurityContextHolder.getContext().getAuthentication();
+  }
+
+  private boolean isUserCreator(Authentication authentication, CommentResponse response) {
+    return authentication.getName().equals(response.getUser().getEmail());
+  }
+
+  private boolean isAdmin(Authentication authentication) {
+    return authentication.getAuthorities()
+        .stream()
+        .map(GrantedAuthority::getAuthority)
+        .anyMatch(authority -> authority.equals(Role.ADMIN.name()));
   }
 
 }
