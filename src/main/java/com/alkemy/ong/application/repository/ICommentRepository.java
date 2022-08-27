aee918a3c16617d40c9a2cb95d20f90e834eb507
Ezequiel Villafañe <eze.villafae07@gmail.com>
2022-08-27 10:55:09 -0300

    OT275-79: Added delete and exist methods to comment repository interface

--- src/main/java/com/alkemy/ong/application/repository/ICommentRepository.java
+++ src/main/java/com/alkemy/ong/application/repository/ICommentRepository.java
@@ -1,6 +1,12 @@
 package com.alkemy.ong.application.repository;
 
 
+import com.alkemy.ong.domain.Identifiable;
+
 public interface ICommentRepository {
+
+  void delete(Identifiable<Long> identifiable);
+
+  boolean exists(Identifiable<Long> identifiable);
 
 }
--- src/main/java/com/alkemy/ong/infrastructure/database/repository/CommentRepository.java
+++ src/main/java/com/alkemy/ong/infrastructure/database/repository/CommentRepository.java
@@ -1,6 +1,7 @@
 package com.alkemy.ong.infrastructure.database.repository;
 
 import com.alkemy.ong.application.repository.ICommentRepository;
+import com.alkemy.ong.domain.Identifiable;
 import com.alkemy.ong.infrastructure.database.repository.abstraction.ICommentSpringRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -11,4 +12,13 @@ public class CommentRepository implements ICommentRepository {
 
   private final ICommentSpringRepository commentSpringRepository;
 
+  @Override
+  public void delete(Identifiable<Long> identifiable) {
+
+  }
+
+  @Override
+  public boolean exists(Identifiable<Long> identifiable) {
+    return false;
+  }
 }
