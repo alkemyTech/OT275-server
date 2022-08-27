ebe5ccc3b81fb947bdc8e4b1f35b186d86b85f40
Liliana <68357688+LAYALA09@users.noreply.github.com>
2022-08-27 18:10:27 -0300

    OT275-71: Delete member endpoint (#24)

---
+++ src/test/java/com/alkemy/ong/application/service/DeleteMemberUseCaseServiceTest.java
@@ -0,0 +1,48 @@
+package com.alkemy.ong.application.service;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import com.alkemy.ong.application.exception.ObjectNotFound;
+import com.alkemy.ong.application.repository.IMemberRepository;
+import com.alkemy.ong.domain.Identifiable;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class DeleteMemberUseCaseServiceTest {
+
+  private DeleteMemberUseCaseService memberService;
+
+  @Mock
+  private IMemberRepository memberRepository;
+
+  @Mock
+  private Identifiable<Long> identifiable;
+
+  @BeforeEach
+  void setup() {
+    memberService = new DeleteMemberUseCaseService(memberRepository);
+  }
+
+  @Test
+  void shouldThrowExceptionWhenMemberDoesNotExist() {
+    given(memberRepository.exists(identifiable)).willReturn(false);
+
+    assertThrows(ObjectNotFound.class, () -> memberService.delete(identifiable));
+  }
+
+  @Test
+  void shouldDeleteMemberWhenMemberExist() {
+    given(memberRepository.exists(identifiable)).willReturn(true);
+
+    memberService.delete(identifiable);
+
+    verify(memberRepository).exists(identifiable);
+    verify(memberRepository).delete(identifiable);
+  }
+
+}
