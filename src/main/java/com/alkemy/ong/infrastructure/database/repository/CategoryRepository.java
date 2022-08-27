3b5731b3ff97a069dc1a2b37e1a8474fc8852730
FedericoRohr <rohrfede@gmail.com>
2022-08-27 15:22:24 -0300

    OT275-45 : added methods delete and exist abstractions and implementations

--- src/main/java/com/alkemy/ong/infrastructure/database/repository/CategoryRepository.java
+++ src/main/java/com/alkemy/ong/infrastructure/database/repository/CategoryRepository.java
@@ -1,6 +1,7 @@
 package com.alkemy.ong.infrastructure.database.repository;
 
 import com.alkemy.ong.application.repository.ICategoryRepository;
+import com.alkemy.ong.domain.Identifiable;
 import com.alkemy.ong.infrastructure.database.repository.abstraction.ICategorySpringRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -11,4 +12,13 @@ public class CategoryRepository implements ICategoryRepository {
 
   private final ICategorySpringRepository categorySpringRepository;
 
+  @Override
+  public void delete(Identifiable<Long> identifiable) {
+    categorySpringRepository.softDelete(identifiable.getId());
+  }
+
+  @Override
+  public boolean exist(Identifiable<Long> identifiable) {
+    return categorySpringRepository.exists(identifiable.getId()).isPresent();
+  }
 }
--- src/main/java/com/alkemy/ong/infrastructure/database/repository/abstraction/ICategorySpringRepository.java
+++ src/main/java/com/alkemy/ong/infrastructure/database/repository/abstraction/ICategorySpringRepository.java
@@ -1,10 +1,21 @@
 package com.alkemy.ong.infrastructure.database.repository.abstraction;
 
 import com.alkemy.ong.infrastructure.database.entity.CategoryEntity;
+import com.alkemy.ong.infrastructure.database.entity.UserEntity;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ICategorySpringRepository extends JpaRepository<CategoryEntity, Long> {
 
+  @Modifying
+  @Query("UPDATE UserEntity C SET C.softDeleted = true WHERE C.userId = :id")
+  void softDelete(@Param("id") Long id);
+
+  @Query(value = "SELECT C FROM UserEntity C WHERE C.softDeleted = false AND C.userId = :id")
+  Optional<UserEntity> exists(@Param("id") Long id);
 }
