0f3a9bd9ba3f93bd59406effdc56c110065bfa68
Ezequiel Villafañe <eze.villafae07@gmail.com>
2022-08-27 17:29:08 -0300

    OT275-79: Added comment and user dto

---
+++ src/main/java/com/alkemy/ong/infrastructure/rest/response/UserResponse.java
@@ -0,0 +1,16 @@
+package com.alkemy.ong.infrastructure.rest.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class UserResponse {
+
+  private String email;
+
+}
