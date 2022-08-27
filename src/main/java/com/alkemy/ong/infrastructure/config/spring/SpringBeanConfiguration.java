81a4236a0162947261a70c031b5ac68305465a0f
Ricardo Olivera <rholivera93@gmail.com>
2022-08-27 00:45:16 -0300

    OT275-60: modify SpringBeanConfiguration

--- src/main/java/com/alkemy/ong/infrastructure/config/spring/SpringBeanConfiguration.java
+++ src/main/java/com/alkemy/ong/infrastructure/config/spring/SpringBeanConfiguration.java
@@ -1,7 +1,10 @@
 package com.alkemy.ong.infrastructure.config.spring;
 
+import com.alkemy.ong.application.repository.ISlideRepository;
 import com.alkemy.ong.application.repository.IUserRepository;
+import com.alkemy.ong.application.service.DeleteSlideUseCaseService;
 import com.alkemy.ong.application.service.DeleteUserUseCaseService;
+import com.alkemy.ong.application.service.usecase.IDeleteSlideUseCase;
 import com.alkemy.ong.application.service.usecase.IDeleteUserUseCase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,5 +15,10 @@ public class SpringBeanConfiguration {
   @Bean
   public IDeleteUserUseCase deleteUserUseCase(IUserRepository userRepository) {
     return new DeleteUserUseCaseService(userRepository);
+  }
+
+  @Bean
+  public IDeleteSlideUseCase deleteSlideUseCase(ISlideRepository slideRepository){
+    return new DeleteSlideUseCaseService(slideRepository);
   }
 }
