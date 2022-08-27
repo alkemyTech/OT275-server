58641a184e251f704747c9f6c97537c07833992b
FedericoRohr <rohrfede@gmail.com>
2022-08-27 19:15:42 -0300

    OT275-45:fixed errors

--- src/main/java/com/alkemy/ong/infrastructure/config/spring/SpringBeanConfiguration.java
+++ src/main/java/com/alkemy/ong/infrastructure/config/spring/SpringBeanConfiguration.java
@@ -1,20 +1,15 @@
 package com.alkemy.ong.infrastructure.config.spring;
 
-
 import com.alkemy.ong.application.repository.ICategoryRepository;
 import com.alkemy.ong.application.repository.IMemberRepository;
 import com.alkemy.ong.application.repository.ISlideRepository;
 import com.alkemy.ong.application.repository.ITestimonialRepository;
 import com.alkemy.ong.application.repository.IUserRepository;
 import com.alkemy.ong.application.service.DeleteCategoryUseCaseService;
-import com.alkemy.ong.application.service.DeleteMemberUseCaseService;
 import com.alkemy.ong.application.service.DeleteSlideUseCaseService;
-import com.alkemy.ong.application.service.DeleteTestimonialUseCaseService;
 import com.alkemy.ong.application.service.DeleteUserUseCaseService;
 import com.alkemy.ong.application.service.usecase.IDeleteCategoryUseCase;
-import com.alkemy.ong.application.service.usecase.IDeleteMemberUseCase;
 import com.alkemy.ong.application.service.usecase.IDeleteSlideUseCase;
-import com.alkemy.ong.application.service.usecase.IDeleteTestimonialUseCase;
 import com.alkemy.ong.application.service.usecase.IDeleteUserUseCase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
