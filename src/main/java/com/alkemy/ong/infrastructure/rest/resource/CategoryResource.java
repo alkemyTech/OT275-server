96113d2f8ba022e027cfbe7601d0c7964ef8e7d6
FedericoRohr <rohrfede@gmail.com>
2022-08-27 16:08:13 -0300

    OT275-45: CategoryController and delete method added

---
+++ src/main/java/com/alkemy/ong/infrastructure/rest/resource/CategoryResource.java
@@ -0,0 +1,25 @@
+package com.alkemy.ong.infrastructure.rest.resource;
+
+import com.alkemy.ong.application.service.usecase.IDeleteCategoryUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/Categories")
+@RestController
+@RequiredArgsConstructor
+public class CategoryResource {
+
+  private final IDeleteCategoryUseCase deleteCategoryUseCase;
+
+@DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+ public ResponseEntity<Void>delete(@PathVariable Long id){
+  deleteCategoryUseCase.delete(() -> id);
+  return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+}
+}
