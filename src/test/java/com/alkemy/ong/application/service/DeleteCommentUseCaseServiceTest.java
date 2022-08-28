7ed5bfd762bb67be12539913d8b6f455629ea4f2
Ezequiel Villafañe <eze.villafae07@gmail.com>
2022-08-27 22:16:18 -0300

    OT275-79: removed public access modifier from DeleteCommentUseCaseServiceTest

--- src/test/java/com/alkemy/ong/application/service/DeleteCommentUseCaseServiceTest.java
+++ src/test/java/com/alkemy/ong/application/service/DeleteCommentUseCaseServiceTest.java
@@ -17,7 +17,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
-public class DeleteCommentUseCaseServiceTest {
+class DeleteCommentUseCaseServiceTest {
 
   private DeleteCommentUseCaseService deleteCommentUseCaseService;
 
