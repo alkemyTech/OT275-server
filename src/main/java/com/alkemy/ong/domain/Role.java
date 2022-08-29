080b1f6e7a74c909d25c08af8787ec74a90e3e3f
Ricardo Olivera <rholivera93@gmail.com>
2022-08-29 15:59:29 -0300

    OT275-25: modify role domain

--- src/main/java/com/alkemy/ong/domain/Role.java
+++ src/main/java/com/alkemy/ong/domain/Role.java
@@ -1,5 +1,16 @@
 package com.alkemy.ong.domain;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
 public class Role {
+
+  private Long id;
+  private String name;
+  private String description;
 
 }
