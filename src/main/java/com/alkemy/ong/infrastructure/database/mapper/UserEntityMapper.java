4ea3c8499a17469cf9839bb6da2c86a37050af87
Ricardo Olivera <rholivera93@gmail.com>
2022-08-29 17:34:41 -0300

    OT275-25: modify user entity mapper

--- src/main/java/com/alkemy/ong/infrastructure/database/mapper/UserEntityMapper.java
+++ src/main/java/com/alkemy/ong/infrastructure/database/mapper/UserEntityMapper.java
@@ -1,18 +1,29 @@
 package com.alkemy.ong.infrastructure.database.mapper;
 
 import com.alkemy.ong.domain.User;
+import com.alkemy.ong.infrastructure.config.spring.security.common.JwtUtils;
 import com.alkemy.ong.infrastructure.database.entity.UserEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class UserEntityMapper {
 
+  private final JwtUtils jwtUtils;
+  private final RoleEntityMapper roleEntityMapper;
+
   public User toDomain(UserEntity entity) {
-    User user = new User();
-
-    user.setEmail(entity.getEmail());
-
-    return user;
+    return User.builder()
+        .email(entity.getEmail())
+        .firstName(entity.getFirstName())
+        .lastName(entity.getLastName())
+        .imageUrl(entity.getImageUrl())
+        .password(entity.getPassword())
+        .softDeleted(entity.isSoftDeleted())
+        .role(roleEntityMapper.toDomain(entity.getRole()))
+        .build();
   }
 
 }
