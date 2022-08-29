132a638fd1afdd520fba037562ccbf531a6eab5a
Ricardo Olivera <rholivera93@gmail.com>
2022-08-29 16:00:41 -0300

    OT275-25: modify user domain

--- src/main/java/com/alkemy/ong/domain/User.java
+++ src/main/java/com/alkemy/ong/domain/User.java
@@ -1,12 +1,21 @@
 package com.alkemy.ong.domain;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+@Builder
 @Getter
 @Setter
 public class User {
 
+  private String firstName;
+  private String lastName;
   private String email;
+  private String password;
+  private String imageUrl;
+  private Role role;
+  private Boolean softDeleted;
 
 }
+
