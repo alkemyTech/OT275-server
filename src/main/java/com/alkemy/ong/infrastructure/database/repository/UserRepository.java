4736e788bfb2903470005ff7895811a4bca4ac60
Ricardo Olivera <rholivera93@gmail.com>
2022-08-29 18:18:35 -0300

    OT275-25: modify user repository

--- src/main/java/com/alkemy/ong/infrastructure/database/repository/UserRepository.java
+++ src/main/java/com/alkemy/ong/infrastructure/database/repository/UserRepository.java
@@ -2,7 +2,13 @@ package com.alkemy.ong.infrastructure.database.repository;
 
 import com.alkemy.ong.application.repository.IUserRepository;
 import com.alkemy.ong.domain.Identifiable;
+import com.alkemy.ong.domain.User;
+import com.alkemy.ong.infrastructure.database.entity.RoleEntity;
+import com.alkemy.ong.infrastructure.database.entity.UserEntity;
+import com.alkemy.ong.infrastructure.database.mapper.UserEntityMapper;
+import com.alkemy.ong.infrastructure.database.repository.abstraction.IRoleSpringRepository;
 import com.alkemy.ong.infrastructure.database.repository.abstraction.IUserSpringRepository;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -11,6 +17,8 @@ import org.springframework.stereotype.Component;
 public class UserRepository implements IUserRepository {
 
   private final IUserSpringRepository userSpringRepository;
+  private final UserEntityMapper userEntityMapper;
+  private final IRoleSpringRepository roleSpringRepository;
 
   @Override
   public void delete(Identifiable<Long> identifiable) {
@@ -22,4 +30,21 @@ public class UserRepository implements IUserRepository {
     return userSpringRepository.exists(identifiable.getId()).isPresent();
   }
 
+  @Override
+  public Optional<User> find(String email) {
+    return userSpringRepository.findByEmail(email).map(userEntityMapper::toDomain);
+  }
+
+  @Override
+  public User add(User user) {
+    UserEntity userEntity = userEntityMapper.toEntity(user);
+    String roleName = user.getRole().getName();
+    userEntity.setRole(getRoleEntity(roleName));
+    userEntity.setSoftDeleted(false);
+    return userEntityMapper.toDomain(userSpringRepository.save(userEntity));
+  }
+
+  private RoleEntity getRoleEntity(String roleName) {
+    return roleSpringRepository.findByName(roleName);
+  }
 }
