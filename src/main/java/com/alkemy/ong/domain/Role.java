1fb34cd80c605d557ab7bfa36951ae91b832ccc8
Ricardo Olivera <rholivera93@gmail.com>
2022-08-29 17:52:53 -0300

    OT275-25: fixed role domain

--- src/main/java/com/alkemy/ong/domain/Role.java
+++ src/main/java/com/alkemy/ong/domain/Role.java
@@ -1,10 +1,13 @@
 package com.alkemy.ong.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 public class Role {
