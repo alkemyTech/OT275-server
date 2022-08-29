61c480fd50044d5ae6c387ee3776189e64b682a4
Sieglind <moraledacd@gmail.com>
2022-08-29 11:17:02 -0300

    Corrected Checkstyle warning

--- src/main/java/com/alkemy/ong/infrastructure/database/repository/abstraction/INewsSpringRepository.java
+++ src/main/java/com/alkemy/ong/infrastructure/database/repository/abstraction/INewsSpringRepository.java
@@ -1,13 +1,13 @@
 package com.alkemy.ong.infrastructure.database.repository.abstraction;
 
 import com.alkemy.ong.infrastructure.database.entity.NewsEntity;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 
 @Repository
 public interface INewsSpringRepository extends JpaRepository<NewsEntity, Long> {
