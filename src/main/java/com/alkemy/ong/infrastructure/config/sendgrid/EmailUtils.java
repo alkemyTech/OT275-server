338d313669ce77f38a9ca087ef29f1fce25f9d1f
Ezequiel Villafañe <eze.villafae07@gmail.com>
2022-08-29 19:33:10 -0300

    OT275-28: implements IMailSend with api sendgrid

---
+++ src/main/java/com/alkemy/ong/infrastructure/config/sendgrid/EmailUtils.java
@@ -0,0 +1,64 @@
+package com.alkemy.ong.infrastructure.config.sendgrid;
+
+import com.alkemy.ong.application.exception.ErrorMessage;
+import com.alkemy.ong.application.exception.ServiceException;
+import com.alkemy.ong.application.util.EmailService;
+import com.alkemy.ong.application.util.IMail;
+import com.alkemy.ong.application.util.IMailSend;
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
+import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class EmailUtils implements IMailSend {
+
+  @Value("${app.email}")
+  private String fromEmail;
+  @Value("${spring.sendgrid.api-key}")
+  private String sendGridApiKey;
+
+
+  @Override
+  public void send(IMail mail) {
+    Mail mailRequest = buildMail(mail);
+
+    SendGrid sendGrid = new SendGrid(sendGridApiKey);
+    Request request = new Request();
+
+    try {
+      request.setMethod(Method.POST);
+      request.setEndpoint("mail/send");
+      request.setBody(mailRequest.build());
+      Response response = sendGrid.api(request);
+      log.info(String.valueOf(response.getStatusCode()));
+      log.info(response.getBody());
+      log.info(String.valueOf(response.getHeaders()));
+    } catch (IOException ex) {
+      log.error(ex.getMessage());
+      throw new ServiceException(ErrorMessage.SERVICE_EMAIL_ERROR.getMessage());
+    }
+
+  }
+
+  @Override
+  public EmailService getServiceName() {
+    return EmailService.SENDGRID;
+  }
+
+  private Mail buildMail(IMail mail) {
+    Email from = new Email(fromEmail);
+    String subject = mail.getSubject();
+    Email to = new Email(mail.getTo());
+    Content content = new Content(mail.getContentType(), mail.getContent());
+    return new Mail(from, subject, to, content);
+  }
+}
