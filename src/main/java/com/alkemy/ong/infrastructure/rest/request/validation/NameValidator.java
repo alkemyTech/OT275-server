f0c57380d039493c96bf35b431418696056f3814
Ricardo Olivera <rholivera93@gmail.com>
2022-08-29 19:11:21 -0300

    OT275-25: added custom annotation for register name validation

---
+++ src/main/java/com/alkemy/ong/infrastructure/rest/request/validation/NameValidator.java
@@ -0,0 +1,23 @@
+package com.alkemy.ong.infrastructure.rest.request.validation;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class NameValidator implements ConstraintValidator<Name, String> {
+
+  private final String REGEXP_NAME = "^\\p{L}+[\\p{L}\\s]*$";
+
+  @Override
+  public void initialize(Name constraintAnnotation) {
+    ConstraintValidator.super.initialize(constraintAnnotation);
+  }
+
+  @Override
+  public boolean isValid(String name, ConstraintValidatorContext constraintValidatorContext) {
+    Pattern pattern = Pattern.compile(REGEXP_NAME);
+    Matcher matcher = pattern.matcher(name);
+    return matcher.matches();
+  }
+}
