a066676cad0fcbc891be912ae13adb2107eaa2eb
Sieglind <moraledacd@gmail.com>
2022-08-29 18:18:26 -0300

    Changed response body construction method, changed error response format for project-wide format, declarede bean "ObjectMapper" and injected it

--- src/main/java/com/alkemy/ong/infrastructure/config/spring/security/filter/AuthEntryPoint.java
+++ src/main/java/com/alkemy/ong/infrastructure/config/spring/security/filter/AuthEntryPoint.java
@@ -1,12 +1,14 @@
 package com.alkemy.ong.infrastructure.config.spring.security.filter;
 
+import com.alkemy.ong.infrastructure.rest.response.ErrorResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.sql.Timestamp;
+import java.io.PrintWriter;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
@@ -15,6 +17,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class AuthEntryPoint implements AuthenticationEntryPoint {
 
+  private static final int STATUS_CODE = 403;
+  private static final String MESSAGE = "FORBIDDEN";
+  private static final List<String> MORE_INFO = List.of("Invalid Credentials");
+
+  @Autowired
+  ObjectMapper objectMapper;
+
   @Override
   public void commence(HttpServletRequest request, HttpServletResponse response,
       AuthenticationException authException) throws IOException, ServletException {
@@ -22,27 +31,10 @@ public class AuthEntryPoint implements AuthenticationEntryPoint {
     response.setContentType(MediaType.APPLICATION_JSON_VALUE);
     response.setStatus(HttpServletResponse.SC_FORBIDDEN);
 
-    OutputStream responseStream = response.getOutputStream();
-    ObjectMapper mapper = new ObjectMapper();
-    mapper.writeValue(responseStream,
-        new AuthenticationError(new Timestamp(System.currentTimeMillis()),
-            request.getServletPath()));
-    responseStream.flush();
-  }
-
-
-  private static class AuthenticationError {
-
-    public Timestamp timestamp;
-    public final int statusCode = 403;
-    public final String error = "Forbidden";
-    public final String message = "Invalid Credentials";
-    public String path;
-
-    public AuthenticationError(Timestamp timestamp, String path) {
-      this.timestamp = timestamp;
-      this.path = path;
-    }
+    PrintWriter writer =  response.getWriter();
+    objectMapper.writeValue(writer,
+        new ErrorResponse(STATUS_CODE,MESSAGE,MORE_INFO));
+    writer.flush();
   }
 
 }
