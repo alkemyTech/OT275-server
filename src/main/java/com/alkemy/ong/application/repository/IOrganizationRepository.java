95bd84222c68e2acd698307f73a8e6b7e2393c19
LAYALA09 <ayalilian2018@gmail.com>
2022-08-30 14:45:59 -0300

    OT275-24:Added Repository

--- src/main/java/com/alkemy/ong/application/repository/IOrganizationRepository.java
+++ src/main/java/com/alkemy/ong/application/repository/IOrganizationRepository.java
@@ -3,9 +3,11 @@ package com.alkemy.ong.application.repository;
 
 import com.alkemy.ong.domain.Organization;
 
+
 public interface IOrganizationRepository {
 
-Organization findById();
+Organization getPublicOrganization();
+
 
 }
 
--- src/main/java/com/alkemy/ong/infrastructure/database/repository/abstraction/IOrganizationSpringRepository.java
+++ src/main/java/com/alkemy/ong/infrastructure/database/repository/abstraction/IOrganizationSpringRepository.java
@@ -1,12 +1,13 @@
 package com.alkemy.ong.infrastructure.database.repository.abstraction;
 
 import com.alkemy.ong.infrastructure.database.entity.OrganizationEntity;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IOrganizationSpringRepository extends JpaRepository<OrganizationEntity, Long> {
 
-  OrganizationEntity finById(Long zero);
+  Optional<OrganizationEntity> findById(Long zero);
 
 }
