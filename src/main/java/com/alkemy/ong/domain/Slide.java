be4eb1a594903a5e575aeebb9d736ed93a9cc6e9
Ricardo Olivera <rholivera93@gmail.com>
2022-08-31 12:17:23 -0300

    OT275-56: added slide domain

---
+++ src/main/java/com/alkemy/ong/domain/Slide.java
@@ -0,0 +1,13 @@
+package com.alkemy.ong.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Slide {
+
+  private String imageUrl;
+  private Integer order;
+
+}
