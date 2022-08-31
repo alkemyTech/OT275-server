3796d27c7b4eb6843811e08b3a1fed0084116992
René Guardia <reneguardia@outlook.com>
2022-08-31 10:58:44 -0300

    OT275-26: Added login endpoint

--- src/main/java/com/alkemy/ong/infrastructure/rest/resource/AuthResource.java
+++ src/main/java/com/alkemy/ong/infrastructure/rest/resource/AuthResource.java
@@ -1,9 +1,13 @@
 package com.alkemy.ong.infrastructure.rest.resource;
 
 import com.alkemy.ong.application.service.usecase.ICreateUserUseCase;
+import com.alkemy.ong.application.service.usecase.ILoginUser;
 import com.alkemy.ong.domain.User;
+import com.alkemy.ong.infrastructure.rest.mapper.AuthenticationMapper;
 import com.alkemy.ong.infrastructure.rest.mapper.UserRegisterMapper;
+import com.alkemy.ong.infrastructure.rest.request.AuthenticationRequest;
 import com.alkemy.ong.infrastructure.rest.request.UserRegisterRequest;
+import com.alkemy.ong.infrastructure.rest.response.AuthenticationResponse;
 import com.alkemy.ong.infrastructure.rest.response.UserRegisterResponse;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -21,7 +25,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthResource {
 
   private final ICreateUserUseCase createUserUseCase;
+
+  private final ILoginUser loginUser;
+
   private final UserRegisterMapper userRegisterMapper;
+
+  private final AuthenticationMapper authMapper;
 
   @PostMapping(
       value = "/register",
@@ -34,4 +43,15 @@ public class AuthResource {
     UserRegisterResponse response = userRegisterMapper.toResponse(savedUser);
     return new ResponseEntity<>(response, HttpStatus.CREATED);
   }
+
+  @PostMapping(
+      value = "/login",
+      produces = MediaType.APPLICATION_JSON_VALUE,
+      consumes = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<AuthenticationResponse> login(
+      @Valid @RequestBody AuthenticationRequest authRequest) {
+    User user = loginUser.login(authMapper.toDomain(authRequest));
+    return ResponseEntity.ok().body(authMapper.toResponse(user));
+  }
+
 }
