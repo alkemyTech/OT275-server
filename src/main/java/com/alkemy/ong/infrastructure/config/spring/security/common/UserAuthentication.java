71270c21bef49338c46ecaf6a5a53f1f055c118a
René Guardia <reneguardia@outlook.com>
2022-08-31 17:58:49 -0300

    OT275-26: Added user authentication

---
+++ src/main/java/com/alkemy/ong/infrastructure/config/spring/security/common/UserAuthentication.java
@@ -0,0 +1,43 @@
+package com.alkemy.ong.infrastructure.config.spring.security.common;
+
+import com.alkemy.ong.application.exception.ErrorMessage;
+import com.alkemy.ong.application.exception.InvalidCredentials;
+import com.alkemy.ong.application.service.usecase.IAuthenticateUser;
+import com.alkemy.ong.domain.User;
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class UserAuthentication implements IAuthenticateUser {
+
+  private AuthenticationManager authenticationManager;
+
+  private UserDetailsService userDetailsService;
+
+  private PasswordEncoder passwordEncoder;
+
+  @Override
+  public void authenticate(User user) {
+    UserDetails userDetails;
+    try {
+      Authentication auth = authenticationManager.authenticate(
+          new UsernamePasswordAuthenticationToken(
+              user.getEmail(),
+              user.getPassword()
+          )
+      );
+      userDetails = (UserDetails) auth.getPrincipal();
+    } catch (Exception e) {
+      throw new InvalidCredentials(ErrorMessage.INVALID_CREDENTIALS.getMessage());
+    }
+    final String token = JwtUtils.create(userDetails);
+    System.out.println(token);
+  }
+
+}
