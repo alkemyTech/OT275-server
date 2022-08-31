70fbb338705b12dc5870e0155ced7656a4aca347
Magali Kain <magali.kain@thoughtworks.com>
2022-09-01 00:27:06 +0200

    Move authenticate implementation into CustomUserDetailsService

--- src/main/java/com/alkemy/ong/infrastructure/config/spring/security/common/CustomUserDetailsService.java
+++ src/main/java/com/alkemy/ong/infrastructure/config/spring/security/common/CustomUserDetailsService.java
@@ -1,11 +1,16 @@
 package com.alkemy.ong.infrastructure.config.spring.security.common;
 
 import com.alkemy.ong.application.exception.ErrorMessage;
+import com.alkemy.ong.application.exception.InvalidCredentials;
 import com.alkemy.ong.application.exception.ObjectNotFound;
+import com.alkemy.ong.application.service.delegate.IAuthenticateUser;
+import com.alkemy.ong.domain.User;
 import com.alkemy.ong.infrastructure.database.entity.UserEntity;
 import com.alkemy.ong.infrastructure.database.repository.abstraction.IUserSpringRepository;
 import java.util.Optional;
 import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -13,9 +18,10 @@ import org.springframework.stereotype.Component;
 
 @AllArgsConstructor
 @Component
-public class CustomUserDetailsService implements UserDetailsService {
+public class CustomUserDetailsService implements UserDetailsService, IAuthenticateUser {
 
-  private IUserSpringRepository userSpringRepository;
+  private final IUserSpringRepository userSpringRepository;
+  private final AuthenticationManager authenticationManager;
 
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -24,5 +30,16 @@ public class CustomUserDetailsService implements UserDetailsService {
       throw new ObjectNotFound(ErrorMessage.OBJECT_NOT_FOUND.getMessage("User"));
     }
     return userEntity.get();
+  }
+
+  @Override
+  public void authenticate(User user) {
+    try {
+      authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+          user.getEmail(),
+          user.getPassword()));
+    } catch (Exception e) {
+      throw new InvalidCredentials(ErrorMessage.INVALID_CREDENTIALS.getMessage());
+    }
   }
 }
