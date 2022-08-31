4a9dbdf759570904785a3c408bbbf33450ffdd55
FedericoRohr <rohrfede@gmail.com>
2022-08-31 12:49:46 -0300

    OT275-53 : refactor ActivitySeeder name and add @component

--- src/main/java/com/alkemy/ong/infrastructure/config/seeder/ActivitySeeder.java
+++ src/main/java/com/alkemy/ong/infrastructure/config/seeder/ActivitySeeder.java
@@ -4,37 +4,37 @@ import com.alkemy.ong.infrastructure.database.entity.ActivityEntity;
 import com.alkemy.ong.infrastructure.database.repository.ActivityRepository;
 import java.util.ArrayList;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.event.ContextStartedEvent;
+import lombok.AllArgsConstructor;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
 
+@AllArgsConstructor
+@Component
+public class ActivitySeeder {
 
-public class Seeder {
-
-  /*
-    @Override
-    public void run(String... args) throws Exception {
-      System.out.println("Hola");
-    }
-
-   */
-  @Autowired
   ActivityRepository activityRepository;
 
   private List<ActivityEntity> activities;
 
-  private void addToList() {
+  private void addActivitiesToList() {
     activities = new ArrayList<>();
+
     activities.add(new ActivityEntity("Elementary School",
         "Elementary School school-based support programmes", "01"));
+
     activities.add(new ActivityEntity("High School",
         "High School school-based support programmes", "01"));
-    activities.add(new ActivityEntity("Lessons", "Math and English lessons",
-        "01"));
+
+    activities.add(new ActivityEntity("Lessons",
+        "Math and English lessons", "01"));
   }
 
-  @EventListener
-  public void seed(ContextStartedEvent asd) {
+  @EventListener()
+  public void seed(ContextRefreshedEvent event) {
+
+    this.addActivitiesToList();
+
     for (ActivityEntity a : activities) {
       if (!activityRepository.existSeed(a.getName())) {
         activityRepository.save(a);
