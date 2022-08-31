c92356bc74d5151d99afd6753dca5fe19ec45931
René Guardia <reneguardia@outlook.com>
2022-08-31 11:00:37 -0300

    OT275-26: Added AuthRequest and Response

---
+++ src/main/java/com/alkemy/ong/infrastructure/rest/request/AuthenticationRequest.java
@@ -0,0 +1,21 @@
+package com.alkemy.ong.infrastructure.rest.request;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AuthenticationRequest {
+
+  @Email(message = "Email has invalid format")
+  private String email;
+
+  @Size(message = "Password must be between 8 and 16 characters long", min = 8, max = 16)
+  private String password;
+
+
+}
+
+
---
+++ src/main/java/com/alkemy/ong/infrastructure/rest/response/AuthenticationResponse.java
@@ -0,0 +1,18 @@
+package com.alkemy.ong.infrastructure.rest.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AuthenticationResponse {
+
+  private String firstName;
+
+  private String lastName;
+
+  private String email;
+
+  private String imageUrl;
+
+}
