dc646fe37eeebda1881bfd8a3441ab8c584d54c2
LAYALA09 <ayalilian2018@gmail.com>
2022-08-31 14:36:45 -0300

    OT275-24:Fixed error CheckStyle

--- src/main/java/com/alkemy/ong/infrastructure/rest/mapper/PublicOrganizationMapper.java
+++ src/main/java/com/alkemy/ong/infrastructure/rest/mapper/PublicOrganizationMapper.java
@@ -1,8 +1,6 @@
 package com.alkemy.ong.infrastructure.rest.mapper;
 
 import com.alkemy.ong.domain.Organization;
-import com.alkemy.ong.domain.User;
-import com.alkemy.ong.infrastructure.rest.request.UserRegisterRequest;
 import com.alkemy.ong.infrastructure.rest.response.PublicOrganizationResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -11,15 +9,15 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class PublicOrganizationMapper {
 
-    public PublicOrganizationResponse toResponse (Organization organization){
-      if (organization == null) {
-        return null;
-      }
-    PublicOrganizationResponse publicOrganizationResponse = new PublicOrganizationResponse();
-      publicOrganizationResponse.setName(organization.getName());
-      publicOrganizationResponse.setImage(organization.getImage());
-      publicOrganizationResponse.setPhone(organization.getPhone());
-      publicOrganizationResponse.setAddress(organization.getAddress());
-      return publicOrganizationResponse;
+  public PublicOrganizationResponse toResponse(Organization organization) {
+    if (organization == null) {
+      return null;
     }
+    PublicOrganizationResponse publicOrganizationResponse = new PublicOrganizationResponse();
+    publicOrganizationResponse.setName(organization.getName());
+    publicOrganizationResponse.setImage(organization.getImage());
+    publicOrganizationResponse.setPhone(organization.getPhone());
+    publicOrganizationResponse.setAddress(organization.getAddress());
+    return publicOrganizationResponse;
   }
+}
