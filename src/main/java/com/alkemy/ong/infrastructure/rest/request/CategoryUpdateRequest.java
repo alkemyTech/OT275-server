c09b021390a9ba382d1ffb42c564edfc1618980d
Sieglind <moraledacd@gmail.com>
2022-08-31 13:13:15 -0300

    OT275-44: Dropped use of custom validation

--- src/main/java/com/alkemy/ong/infrastructure/rest/request/CategoryUpdateRequest.java
+++ src/main/java/com/alkemy/ong/infrastructure/rest/request/CategoryUpdateRequest.java
@@ -2,13 +2,16 @@ package com.alkemy.ong.infrastructure.rest.request;
 
 import com.alkemy.ong.infrastructure.rest.request.validation.Name;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 import lombok.Getter;
 
 @Getter
 public class CategoryUpdateRequest {
 
   @NotEmpty
-  @Name(message = "Category name must contain only spaces and letters.")
+  @Pattern(
+      regexp = "^\\p{L}+[\\p{L}\\s]*$",
+      message = "Category name must contain only spaces and letters.")
   private String name;
   private String description;
   private String imageUrl;
