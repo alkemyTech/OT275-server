00bd46895b0751c05c05527063960cc6825935f2
Sieglind <moraledacd@gmail.com>
2022-08-30 23:53:32 -0300

    OT275-44: Added validation constraints annotations in "CategoryUpdateRequest"

--- src/main/java/com/alkemy/ong/infrastructure/rest/request/CategoryUpdateRequest.java
+++ src/main/java/com/alkemy/ong/infrastructure/rest/request/CategoryUpdateRequest.java
@@ -1,10 +1,14 @@
 package com.alkemy.ong.infrastructure.rest.request;
 
+import com.alkemy.ong.infrastructure.rest.request.validation.Name;
+import javax.validation.constraints.NotEmpty;
 import lombok.Getter;
 
 @Getter
 public class CategoryUpdateRequest {
 
+  @NotEmpty
+  @Name(message = "Category name must contain only spaces and letters.")
   private String name;
   private String description;
   private String imageUrl;
