49d37351bf9f42ce7428e1690be6fa7593baecdd
Magali Kain <magali.kain@thoughtworks.com>
2022-08-31 23:02:58 +0200

    Pr review

--- src/main/java/com/alkemy/ong/infrastructure/rest/mapper/ListSlideMapper.java
+++ src/main/java/com/alkemy/ong/infrastructure/rest/mapper/ListSlideMapper.java
@@ -2,11 +2,8 @@ package com.alkemy.ong.infrastructure.rest.mapper;
 
 import com.alkemy.ong.domain.Slide;
 import com.alkemy.ong.infrastructure.rest.response.ListSlideResponse;
-import com.alkemy.ong.infrastructure.rest.response.SlideResponse;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -20,11 +17,6 @@ public class ListSlideMapper {
     if (slides == null || slides.isEmpty()) {
       return new ListSlideResponse(Collections.emptyList());
     }
-
-    List<SlideResponse> slideResponses = slides.stream()
-        .map(slideMapper::toResponse)
-        .collect(Collectors.toCollection(() -> new ArrayList<>(slides.size())));
-
-    return new ListSlideResponse(slideResponses);
+    return new ListSlideResponse(slideMapper.toResponse(slides));
   }
 }
