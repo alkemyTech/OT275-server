31ce0e536cea96fc50af5572026c7395f25ea083
Ezequiel Villafañe <eze.villafae07@gmail.com>
2022-09-01 20:53:06 -0300

    OT275-38: Added update mapper

--- src/main/java/com/alkemy/ong/infrastructure/database/mapper/OrganizationMapper.java
+++ src/main/java/com/alkemy/ong/infrastructure/database/mapper/OrganizationMapper.java
@@ -19,6 +19,46 @@ public class OrganizationMapper {
     organization.setImage(entity.getImageUrl());
     organization.setPhone(entity.getPhone());
     organization.setAddress(entity.getAddress());
+    organization.setEmail(entity.getEmail());
+    organization.setFacebookUrl(entity.getFacebookUrl());
+    organization.setLinkedInUrl(entity.getLinkedInUrl());
+    organization.setInstagramUrl(entity.getInstagramUrl());
+    organization.setAboutUsText(entity.getAboutUsText());
+    organization.setWelcomeText(entity.getWelcomeText());
     return organization;
+  }
+
+  public void update(OrganizationEntity entity, Organization organization) {
+    if (organization.getName() != null) {
+      entity.setName(organization.getName());
+    }
+    if (organization.getImage() != null) {
+      entity.setImageUrl(organization.getImage());
+    }
+    if (organization.getPhone() != null) {
+      entity.setPhone(organization.getPhone());
+    }
+    if (organization.getAddress() != null) {
+      entity.setAddress(organization.getAddress());
+    }
+    if (organization.getEmail() != null) {
+      entity.setEmail(organization.getEmail());
+    }
+    if (organization.getFacebookUrl() != null) {
+      entity.setFacebookUrl(organization.getFacebookUrl());
+    }
+    if (organization.getLinkedInUrl() != null) {
+      entity.setLinkedInUrl(organization.getLinkedInUrl());
+    }
+    if (organization.getInstagramUrl() != null) {
+      entity.setLinkedInUrl(organization.getLinkedInUrl());
+    }
+    if (organization.getAboutUsText() != null) {
+      entity.setAboutUsText(organization.getAboutUsText());
+    }
+    if (organization.getWelcomeText() != null) {
+      entity.setWelcomeText(organization.getWelcomeText());
+    }
+
   }
 }
