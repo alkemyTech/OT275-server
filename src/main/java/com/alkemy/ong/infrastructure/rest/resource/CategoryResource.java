badf14fb55d50c82f7b9ebbcf75c430311297724
FedericoRohr <rohrfede@gmail.com>
2022-09-01 13:25:00 -0300

    Ot275-42 : added get method in CategoryResource

--- src/main/java/com/alkemy/ong/infrastructure/rest/resource/CategoryResource.java
+++ src/main/java/com/alkemy/ong/infrastructure/rest/resource/CategoryResource.java
@@ -1,11 +1,16 @@
 package com.alkemy.ong.infrastructure.rest.resource;
 
 import com.alkemy.ong.application.service.usecase.IDeleteCategoryUseCase;
+import com.alkemy.ong.application.service.usecase.IGetCategoryUseCase;
+import com.alkemy.ong.domain.Category;
+import com.alkemy.ong.infrastructure.rest.mapper.GetCategoryMapper;
+import com.alkemy.ong.infrastructure.rest.response.GetCategoryResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,9 +22,20 @@ public class CategoryResource {
 
   private final IDeleteCategoryUseCase deleteCategoryUseCase;
 
+  private final IGetCategoryUseCase categoryUseCase;
+
+  private final GetCategoryMapper getCategoryMapper;
+
   @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<Void> delete(@PathVariable Long id) {
     deleteCategoryUseCase.delete(() -> id);
     return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
   }
+
+  @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<GetCategoryResponse> get(@PathVariable Long id) {
+    Category category = categoryUseCase.get(() -> id);
+    return new ResponseEntity<>(getCategoryMapper.toResponse(category), HttpStatus.OK);
+  }
+
 }
