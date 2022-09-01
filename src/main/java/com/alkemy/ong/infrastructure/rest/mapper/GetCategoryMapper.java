2f8aa8fa51c0a65bb7cb367f4faf5a506a28987f
FedericoRohr <rohrfede@gmail.com>
2022-09-01 13:24:00 -0300

    OT275-42 : added GetCategoryMapper and method toResponse

---
+++ src/main/java/com/alkemy/ong/infrastructure/rest/mapper/GetCategoryMapper.java
@@ -0,0 +1,21 @@
+package com.alkemy.ong.infrastructure.rest.mapper;
+
+import com.alkemy.ong.domain.Category;
+import com.alkemy.ong.infrastructure.rest.response.GetCategoryResponse;
+
+public class GetCategoryMapper {
+
+  public GetCategoryResponse toResponse(Category category) {
+    if (category == null) {
+      return null;
+    }
+
+    GetCategoryResponse categoryResponse = new GetCategoryResponse();
+    categoryResponse.setName(categoryResponse.getName());
+    categoryResponse.setContent(categoryResponse.getContent());
+    categoryResponse.setImageUrl(categoryResponse.getImageUrl());
+
+    return categoryResponse;
+  }
+
+}
