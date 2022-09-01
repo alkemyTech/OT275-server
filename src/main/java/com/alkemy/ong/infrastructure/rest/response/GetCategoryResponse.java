472e0f4e104799c419b4e4fdefd62f3abb325654
FedericoRohr <rohrfede@gmail.com>
2022-09-01 13:21:58 -0300

    OT275-42 : added GetCateoryResponse

---
+++ src/main/java/com/alkemy/ong/infrastructure/rest/response/GetCategoryResponse.java
@@ -0,0 +1,19 @@
+package com.alkemy.ong.infrastructure.rest.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetCategoryResponse {
+
+  private String content;
+
+  private String imageUrl;
+
+  private String name;
+}
