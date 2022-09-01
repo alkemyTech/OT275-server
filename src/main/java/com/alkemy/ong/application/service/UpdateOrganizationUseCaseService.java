c01f10a665482bcacea02ae4573ca76b55697e61
Ezequiel Villafañe <eze.villafae07@gmail.com>
2022-09-01 20:54:54 -0300

    OT275-38: Added update organization service

---
+++ src/main/java/com/alkemy/ong/application/service/UpdateOrganizationUseCaseService.java
@@ -0,0 +1,20 @@
+package com.alkemy.ong.application.service;
+
+import com.alkemy.ong.application.repository.IOrganizationRepository;
+import com.alkemy.ong.application.service.usecase.IUpdateOrganizationUseCase;
+import com.alkemy.ong.domain.Organization;
+import javax.transaction.Transactional;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class UpdateOrganizationUseCaseService implements IUpdateOrganizationUseCase {
+
+  private IOrganizationRepository organizationRepository;
+
+  @Transactional
+  @Override
+  public Organization update(Organization organization) {
+    return organizationRepository.update(organization);
+  }
+
+}
