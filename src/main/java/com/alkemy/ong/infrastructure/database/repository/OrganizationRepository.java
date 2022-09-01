4898d8ca6723a66465cd1a5627fba3b76209a534
Ezequiel Villafañe <eze.villafae07@gmail.com>
2022-09-01 20:54:24 -0300

    OT275-38: Added update method to repository implementation

--- src/main/java/com/alkemy/ong/infrastructure/database/repository/OrganizationRepository.java
+++ src/main/java/com/alkemy/ong/infrastructure/database/repository/OrganizationRepository.java
@@ -2,6 +2,7 @@ package com.alkemy.ong.infrastructure.database.repository;
 
 import com.alkemy.ong.application.repository.IOrganizationRepository;
 import com.alkemy.ong.domain.Organization;
+import com.alkemy.ong.infrastructure.database.entity.OrganizationEntity;
 import com.alkemy.ong.infrastructure.database.mapper.OrganizationMapper;
 import com.alkemy.ong.infrastructure.database.repository.abstraction.IOrganizationSpringRepository;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +17,14 @@ public class OrganizationRepository implements IOrganizationRepository {
 
   public Organization getOrganization() {
     return organizationMapper.toDomain(organizationSpringRepository.findAll().get(0));
+  }
+
+  @Override
+  public Organization update(Organization organization) {
+    OrganizationEntity organizationEntity = organizationSpringRepository.findAll().get(0);
+    organizationMapper.update(organizationEntity, organization);
+    return organizationMapper.toDomain(
+        organizationSpringRepository.save(organizationEntity));
   }
 
 }
