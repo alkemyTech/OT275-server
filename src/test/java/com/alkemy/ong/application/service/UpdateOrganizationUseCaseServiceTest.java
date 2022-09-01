c335c3b0e18a4749379c007cf525df715d5414a0
Ezequiel Villafañe <eze.villafae07@gmail.com>
2022-09-01 20:56:25 -0300

    OT275-38: Added unit test from update organization

---
+++ src/test/java/com/alkemy/ong/application/service/UpdateOrganizationUseCaseServiceTest.java
@@ -0,0 +1,39 @@
+package com.alkemy.ong.application.service;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+import com.alkemy.ong.application.repository.IOrganizationRepository;
+import com.alkemy.ong.domain.Organization;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class UpdateOrganizationUseCaseServiceTest {
+
+  private UpdateOrganizationUseCaseService updateOrganizationUseCaseService;
+
+  @Mock
+  private IOrganizationRepository organizationRepository;
+
+  @Mock
+  private Organization organization;
+
+  @BeforeEach
+  void setUp() {
+    updateOrganizationUseCaseService = new UpdateOrganizationUseCaseService(organizationRepository);
+  }
+
+  @Test
+  void shouldUpdateOrganization() {
+    given(organizationRepository.update(organization)).willReturn(organization);
+
+    updateOrganizationUseCaseService.update(organization);
+
+    verify(organizationRepository).update(organization);
+  }
+
+}
