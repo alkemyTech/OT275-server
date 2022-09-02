ca312bc2b0a3c9a180d9e487852601317cc840b3
Ezequiel Villafañe <eze.villafae07@gmail.com>
2022-09-02 19:30:53 -0300

    OT275-61: Added unit test from get organization service

---
+++ src/test/java/com/alkemy/ong/application/service/GetOrganizationUseCaseServiceTest.java
@@ -0,0 +1,46 @@
+package com.alkemy.ong.application.service;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+import com.alkemy.ong.application.repository.IOrganizationRepository;
+import com.alkemy.ong.application.repository.ISlideRepository;
+import com.alkemy.ong.domain.Organization;
+import java.util.Collections;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class GetOrganizationUseCaseServiceTest {
+
+  private GetOrganizationUseCaseService getOrganizationUseCaseService;
+
+  @Mock
+  private IOrganizationRepository organizationRepository;
+
+  @Mock
+  private ISlideRepository slideRepository;
+
+  @Mock
+  private Organization organization;
+
+  @BeforeEach
+  void setUp() {
+    getOrganizationUseCaseService = new GetOrganizationUseCaseService(organizationRepository,
+        slideRepository);
+  }
+
+  @Test
+  void shouldGetOrganization() {
+    given(organizationRepository.getOrganization()).willReturn(organization);
+    given(slideRepository.findAllSortedByPosition()).willReturn(Collections.emptyList());
+
+    getOrganizationUseCaseService.get();
+
+    verify(organizationRepository).getOrganization();
+    verify(slideRepository).findAllSortedByPosition();
+  }
+}
