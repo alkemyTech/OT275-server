0c68a9bee049c1a35cb1df9ac8d7bcedd27df5df
LAYALA09 <ayalilian2018@gmail.com>
2022-09-01 23:32:31 -0300

    save last changes

---
+++ src/main/java/com/alkemy/ong/application/service/usecase/ICreateCategoryUseCase.java
@@ -0,0 +1,9 @@
+package com.alkemy.ong.application.service.usecase;
+
+import com.alkemy.ong.domain.Category;
+
+public interface ICreateCategoryUseCase {
+
+  Category post(Category category);
+
+}
---
+++ src/main/java/com/alkemy/ong/infrastructure/rest/response/CategoryPostResponse.java
@@ -0,0 +1,18 @@
+package com.alkemy.ong.infrastructure.rest.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryPostResponse {
+
+  private String name;
+  private String description;
+  private String image;
+
+}
