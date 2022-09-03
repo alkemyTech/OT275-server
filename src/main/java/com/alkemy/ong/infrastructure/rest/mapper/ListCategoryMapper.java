713a51e06dab47d1f8a98ef31a4074d1ece47958
FedericoRohr <rohrfede@gmail.com>
2022-09-02 21:03:01 -0300

    OT275-41 : added ShortCategoryMapper and ListCategoryMapper

---
+++ src/main/java/com/alkemy/ong/infrastructure/rest/mapper/ListCategoryMapper.java
@@ -0,0 +1,29 @@
+package com.alkemy.ong.infrastructure.rest.mapper;
+
+import com.alkemy.ong.domain.Category;
+import com.alkemy.ong.infrastructure.rest.response.CategoryShortResponse;
+import com.alkemy.ong.infrastructure.rest.response.ListCategoryResponse;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class ListCategoryMapper {
+
+  private final ShortCategoryMapper shortCategoryMapper;
+
+  public ListCategoryResponse toResponse(List<Category> categoryList){
+    if(categoryList== null || categoryList.isEmpty()){
+      return new ListCategoryResponse(Collections.emptyList());
+    }
+    List<CategoryShortResponse>categories = new ArrayList<>();
+    for(Category category : categoryList){
+      categories.add(shortCategoryMapper.toResponse(category));
+    }
+    return new ListCategoryResponse(categories);
+  }
+
+}
