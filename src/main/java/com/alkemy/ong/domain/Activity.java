64a9a27aee5e9b78191b1602e6d290113abe121d
René Guardia <reneguardia@outlook.com>
2022-09-03 18:23:44 -0300

    OT275-52: Added Activity domain object

---
+++ src/main/java/com/alkemy/ong/domain/Activity.java
@@ -0,0 +1,22 @@
+package com.alkemy.ong.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Activity {
+
+  private Long id;
+
+  private String content;
+
+  private String imageUrl;
+
+  private String name;
+
+}
