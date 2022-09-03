aaca292db72c15de3f85158dda5065228a23759f
René Guardia <reneguardia@outlook.com>
2022-09-03 18:37:51 -0300

    OT275-52: Implements exist and update in activity repositories

--- src/main/java/com/alkemy/ong/infrastructure/database/repository/ActivityRepository.java
+++ src/main/java/com/alkemy/ong/infrastructure/database/repository/ActivityRepository.java
@@ -1,7 +1,10 @@
 package com.alkemy.ong.infrastructure.database.repository;
 
 import com.alkemy.ong.application.repository.IActivityRepository;
+import com.alkemy.ong.domain.Activity;
+import com.alkemy.ong.domain.Identifiable;
 import com.alkemy.ong.infrastructure.database.entity.ActivityEntity;
+import com.alkemy.ong.infrastructure.database.mapper.ActivityEntityMapper;
 import com.alkemy.ong.infrastructure.database.repository.abstraction.IActivitySpringRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +16,8 @@ public class ActivityRepository implements IActivityRepository {
 
   private final IActivitySpringRepository activitySpringRepository;
 
+  private final ActivityEntityMapper activityEntityMapper;
+
   public void saveAll(List<ActivityEntity> activities) {
     activitySpringRepository.saveAll(activities);
   }
@@ -20,4 +25,16 @@ public class ActivityRepository implements IActivityRepository {
   public long count() {
     return activitySpringRepository.count();
   }
+
+  @Override
+  public boolean exists(Identifiable<Long> identifiable) {
+    return activitySpringRepository.exists(identifiable.getId()).isPresent();
+  }
+
+  @Override
+  public Activity update(Activity activity) {
+    ActivityEntity activityEntity = activityEntityMapper.toEntity(activity);
+    return activityEntityMapper.toDomain(activitySpringRepository.save(activityEntity));
+  }
+
 }
