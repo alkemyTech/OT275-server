3dc610bbc8850d74eccf962dcdd80aef6c12ce3b
Magali Kain <magali.kain@thoughtworks.com>
2022-09-04 20:53:03 +0200

    Review PR

--- src/main/java/com/alkemy/ong/application/util/template/WelcomeEmailTemplate.java
+++ src/main/java/com/alkemy/ong/application/util/template/WelcomeEmailTemplate.java
@@ -13,6 +13,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class WelcomeEmailTemplate implements IMail {
 
+  private static final String TEXT_HTML = "text/html";
+  private static final String TEMPLATE_HTML = "template/welcome-email-template.html";
   private final IAddressContact addressContact;
   private final Organization organization;
 
@@ -33,36 +35,32 @@ public class WelcomeEmailTemplate implements IMail {
 
   @Override
   public String getContentType() {
-    return "text/html";
+    return TEXT_HTML;
   }
 
   private String setContent() throws IOException {
-    InputStream file = WelcomeEmailTemplate.class.getClassLoader()
-        .getResourceAsStream("template/welcome-email-template.html");
-    InputStreamReader streamReader = null;
-    if (file != null) {
-      streamReader = new InputStreamReader(file);
+    try (BufferedReader reader = new BufferedReader(new InputStreamReader(getFileFromResource()))) {
+      return replacePlaceHolders(reader.readLine());
     }
-    BufferedReader reader = new BufferedReader(streamReader);
-    return replacePlaceHolders(reader.readLine());
   }
 
-  public String replacePlaceHolders(String readFile) {
+  private static InputStream getFileFromResource() {
+    return WelcomeEmailTemplate.class.getClassLoader().getResourceAsStream(TEMPLATE_HTML);
+  }
+
+  public String replacePlaceHolders(String htmlContent) {
+    String html = htmlContent.replace(PlaceHolder.LOGO.name(), organization.getImage())
+        .replace(PlaceHolder.ORG_NAME.name(), organization.getName())
+        .replace(PlaceHolder.WELCOME_TEXT.name(), organization.getWelcomeText())
+        .replace(PlaceHolder.ORG_EMAIL.name(), organization.getEmail())
+        .replace(PlaceHolder.ORG_ADDRESS.name(), organization.getAddress())
+        .replace(PlaceHolder.ORG_PHONE.name(), organization.getPhone());
+
     SocialMedia socialMedia = organization.getSocialMedia();
-    String html = "";
-    if (socialMedia != null && !readFile.isEmpty()) {
-      html += readFile.replace(PlaceHolder.LOGO.name(), organization.getImage())
-          .replace(PlaceHolder.ORG_NAME.name(), organization.getName())
-          .replace(PlaceHolder.WELCOME_TEXT.name(), organization.getWelcomeText())
-          .replace(PlaceHolder.ORG_EMAIL.name(), organization.getEmail())
-          .replace(PlaceHolder.ORG_ADDRESS.name(), organization.getAddress())
-          .replace(PlaceHolder.ORG_PHONE.name(), organization.getPhone())
-          .replace(PlaceHolder.INSTAGRAM_URL.name(),
-              organization.getSocialMedia().getInstagramUrl())
-          .replace(PlaceHolder.FACEBOOK_URL.name(),
-              organization.getSocialMedia().getFacebookUrl())
-          .replace(PlaceHolder.LINKEDIN_URL.name(),
-              organization.getSocialMedia().getLinkedInUrl());
+    if (socialMedia != null) {
+      html = html.replace(PlaceHolder.INSTAGRAM_URL.name(), socialMedia.getInstagramUrl())
+          .replace(PlaceHolder.FACEBOOK_URL.name(), socialMedia.getFacebookUrl())
+          .replace(PlaceHolder.LINKEDIN_URL.name(), socialMedia.getLinkedInUrl());
     }
     return html;
   }
@@ -73,7 +71,8 @@ public class WelcomeEmailTemplate implements IMail {
     ORG_NAME,
     ORG_ADDRESS,
     WELCOME_TEXT,
-    ORG_PHONE, ORG_EMAIL,
+    ORG_PHONE,
+    ORG_EMAIL,
     LINKEDIN_URL,
     FACEBOOK_URL,
     INSTAGRAM_URL
