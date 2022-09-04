d64d6a08445a6f097d13d5373a0aa979f53f4e5e
LAYALA09 <ayalilian2018@gmail.com>
2022-09-04 19:18:37 -0300

    OT275-43: Delete CategoryAlreadyExists

--- src/main/java/com/alkemy/ong/application/exception/CategoryAlreadyExists.java
+++
@@ -1,9 +0,0 @@
-package com.alkemy.ong.application.exception;
-
-
-public class CategoryAlreadyExists extends RuntimeException {
-
-  public CategoryAlreadyExists(String message) {
-    super(message);
-  }
-}
