8a6a95be7ae54a7322820bd0148e43a63eb66023
Sieglind <moraledacd@gmail.com>
2022-09-04 17:24:59 -0300

    OT275-76: Added application bean to configuration

--- src/main/java/com/alkemy/ong/infrastructure/config/spring/SpringBeanConfiguration.java
+++ src/main/java/com/alkemy/ong/infrastructure/config/spring/SpringBeanConfiguration.java
@@ -20,6 +20,7 @@ import com.alkemy.ong.application.service.DeleteUserUseCaseService;
 import com.alkemy.ong.application.service.GetCategoryUseCase;
 import com.alkemy.ong.application.service.GetOrganizationUseCaseService;
 import com.alkemy.ong.application.service.GetSlideUseCaseService;
+import com.alkemy.ong.application.service.ListCommentUserCaseService;
 import com.alkemy.ong.application.service.ListSlideUseCaseService;
 import com.alkemy.ong.application.service.LoginUserUseCaseService;
 import com.alkemy.ong.application.service.UpdateCategoryUserCaseService;
@@ -37,6 +38,7 @@ import com.alkemy.ong.application.service.usecase.IDeleteUserUseCase;
 import com.alkemy.ong.application.service.usecase.IGetCategoryUseCase;
 import com.alkemy.ong.application.service.usecase.IGetOrganizationUseCase;
 import com.alkemy.ong.application.service.usecase.IGetSlideUseCase;
+import com.alkemy.ong.application.service.usecase.IListCommentUseCase;
 import com.alkemy.ong.application.service.usecase.IListSlideUseCase;
 import com.alkemy.ong.application.service.usecase.ILoginUserUseCase;
 import com.alkemy.ong.application.service.usecase.IUpdateCategoryUseCase;
@@ -88,6 +90,11 @@ public class SpringBeanConfiguration {
   }
 
   @Bean
+  public IListCommentUseCase listCommentUseCase(ICommentRepository commentRepository) {
+    return new ListCommentUserCaseService(commentRepository);
+  }
+
+  @Bean
   public IDeleteCommentUseCase deleteCommentUseCase(ICommentRepository commentRepository,
       IOperationAllowed authorization) {
     return new DeleteCommentUseCaseService(commentRepository, authorization);
@@ -125,7 +132,7 @@ public class SpringBeanConfiguration {
   public IGetSlideUseCase getSlideUseCase(ISlideRepository slideRepository) {
     return new GetSlideUseCaseService(slideRepository);
   }
-  
+
   @Bean
   public IUpdateOrganizationUseCase updateOrganizationUseCase(
       IOrganizationRepository organizationRepository) {
