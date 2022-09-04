2a9dc482b55080a77b9aca3692d97775df333f10
Sieglind <moraledacd@gmail.com>
2022-09-04 17:27:28 -0300

    OT275-76: Created Endpoint

--- src/main/java/com/alkemy/ong/infrastructure/rest/resource/CommentResource.java
+++ src/main/java/com/alkemy/ong/infrastructure/rest/resource/CommentResource.java
@@ -1,11 +1,15 @@
 package com.alkemy.ong.infrastructure.rest.resource;
 
 import com.alkemy.ong.application.service.usecase.IDeleteCommentUseCase;
+import com.alkemy.ong.application.service.usecase.IListCommentUseCase;
+import com.alkemy.ong.infrastructure.rest.mapper.ListCommentMapper;
+import com.alkemy.ong.infrastructure.rest.response.ListCommentResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +20,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class CommentResource {
 
   private final IDeleteCommentUseCase deleteCommentUseCase;
+  private final IListCommentUseCase listCommentUseCase;
+  private final ListCommentMapper listCommentMapper;
 
   @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<Void> delete(@PathVariable Long id) {
@@ -23,4 +29,9 @@ public class CommentResource {
     return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
   }
 
+  @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<ListCommentResponse> list() {
+    return ResponseEntity.ok().body(listCommentMapper
+        .toResponse(listCommentUseCase.findAllOrdered()));
+  }
 }
