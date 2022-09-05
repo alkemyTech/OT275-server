f4650919e2bc1ca3750308fcf306532085f17d48
LAYALA09 <ayalilian2018@gmail.com>
2022-09-05 19:16:37 -0300

    OT275-36: Added IListUserUseCase, ListUserResponse and ListUserUseCaseService

--- src/main/java/com/alkemy/ong/infrastructure/rest/response/ListUserResponse.java
+++ src/main/java/com/alkemy/ong/infrastructure/rest/response/ListUserResponse.java
@@ -1,11 +1,13 @@
 package com.alkemy.ong.infrastructure.rest.response;
 
 import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class ListUserResponse {
 
   private List<UserResponse> users;
