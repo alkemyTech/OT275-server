d09c2598ca751b27db6d75b386c9f70f03cc519a
Ricardo Olivera <rholivera93@gmail.com>
2022-09-05 13:10:47 -0300

    OT275-35: refactor update user case service

--- src/main/java/com/alkemy/ong/application/service/UpdateUserUseCaseService.java
+++ src/main/java/com/alkemy/ong/application/service/UpdateUserUseCaseService.java
@@ -15,26 +15,18 @@ public class UpdateUserUseCaseService implements IUpdateUserUseCase {
 
   @Override
   public User update(User user) {
-    Optional<User> savedUserOptional = userRepository.findById(user::getId);
-    if (!userRepository.exists(user::getId) || savedUserOptional.isEmpty())  {
+    Optional<User> userOptional = userRepository.findById(user::getId);
+    if (!userRepository.exists(user::getId) || userOptional.isEmpty()) {
       throw new ObjectNotFoundException(ErrorMessage.OBJECT_NOT_FOUND.getMessage("User"));
     }
-    updateUserData(user, savedUserOptional.get());
-    return userRepository.update(savedUserOptional.get());
+    return userRepository.update(updateValues(user, userOptional.get()));
   }
 
-  private void updateUserData(User user, User savedUser) {
-    updateUserAuthenticationData(user, savedUser);
-    updateUserPersonalData(user, savedUser);
-  }
-
-  private void updateUserAuthenticationData(User user, User savedUser) {
-    savedUser.setPassword(user.getPassword());
-  }
-
-  private void updateUserPersonalData(User user, User savedUser) {
-    savedUser.setFirstName(user.getFirstName());
-    savedUser.setLastName(user.getLastName());
-    savedUser.setImageUrl(user.getImageUrl());
+  private static User updateValues(User user, User updatedUser) {
+    updatedUser.setLastName(user.getLastName());
+    updatedUser.setFirstName(user.getFirstName());
+    updatedUser.setPassword(user.getPassword());
+    updatedUser.setImageUrl(user.getImageUrl());
+    return updatedUser;
   }
 }
