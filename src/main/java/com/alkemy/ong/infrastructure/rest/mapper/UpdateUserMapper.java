eb46702b090695bef824a5e87de2ad281bc1c79e
Magali Kain <magali.kain@thoughtworks.com>
2022-09-05 21:26:17 +0200

    Rename mapper to be consistent with DTO names

--- src/main/java/com/alkemy/ong/infrastructure/rest/mapper/UpdateUserMapper.java
+++ src/main/java/com/alkemy/ong/infrastructure/rest/mapper/UpdateUserMapper.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class UserUpdateMapper {
+public class UpdateUserMapper {
 
   private final PasswordEncoder passwordEncoder;
 
--- src/main/java/com/alkemy/ong/infrastructure/rest/resource/UserResource.java
+++ src/main/java/com/alkemy/ong/infrastructure/rest/resource/UserResource.java
@@ -3,7 +3,7 @@ package com.alkemy.ong.infrastructure.rest.resource;
 import com.alkemy.ong.application.service.usecase.IDeleteUserUseCase;
 import com.alkemy.ong.application.service.usecase.IUpdateUserUseCase;
 import com.alkemy.ong.domain.User;
-import com.alkemy.ong.infrastructure.rest.mapper.UserUpdateMapper;
+import com.alkemy.ong.infrastructure.rest.mapper.UpdateUserMapper;
 import com.alkemy.ong.infrastructure.rest.request.UpdateUserRequest;
 import com.alkemy.ong.infrastructure.rest.response.UpdateUserResponse;
 import javax.validation.Valid;
@@ -25,7 +25,7 @@ public class UserResource {
 
   private final IDeleteUserUseCase deleteUserUseCase;
   private final IUpdateUserUseCase updateUserUseCase;
-  private final UserUpdateMapper userUpdateMapper;
+  private final UpdateUserMapper updateUserMapper;
 
   @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<Void> delete(@PathVariable Long id) {
@@ -38,9 +38,8 @@ public class UserResource {
       consumes = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<UpdateUserResponse> update(@PathVariable Long id,
       @Valid @RequestBody UpdateUserRequest updateUserRequest) {
-    User user = userUpdateMapper.toDomain(() -> id, updateUserRequest);
-    User userUpdated = updateUserUseCase.update(user);
-    UpdateUserResponse response = userUpdateMapper.toResponse(userUpdated);
+    User user = updateUserMapper.toDomain(() -> id, updateUserRequest);
+    UpdateUserResponse response = updateUserMapper.toResponse(updateUserUseCase.update(user));
     return ResponseEntity.ok(response);
   }
 }
