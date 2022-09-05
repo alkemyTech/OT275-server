a0ec887b910dd5c02364ab695ecfca8210d19272
Ricardo Olivera <rholivera93@gmail.com>
2022-09-05 14:05:09 -0300

    OT275-35: modify bean config

--- src/main/java/com/alkemy/ong/infrastructure/config/spring/SpringBeanConfiguration.java
+++ src/main/java/com/alkemy/ong/infrastructure/config/spring/SpringBeanConfiguration.java
@@ -28,6 +28,7 @@ import com.alkemy.ong.application.service.LoginUserUseCaseService;
 import com.alkemy.ong.application.service.UpdateActivityUseCaseService;
 import com.alkemy.ong.application.service.UpdateCategoryUserCaseService;
 import com.alkemy.ong.application.service.UpdateOrganizationUseCaseService;
+import com.alkemy.ong.application.service.UpdateUserUseCaseService;
 import com.alkemy.ong.application.service.delegate.IAuthenticationManager;
 import com.alkemy.ong.application.service.delegate.IOperationAllowed;
 import com.alkemy.ong.application.service.usecase.ICreateCategoryUseCase;
@@ -48,6 +49,7 @@ import com.alkemy.ong.application.service.usecase.ILoginUserUseCase;
 import com.alkemy.ong.application.service.usecase.IUpdateActivityUseCase;
 import com.alkemy.ong.application.service.usecase.IUpdateCategoryUseCase;
 import com.alkemy.ong.application.service.usecase.IUpdateOrganizationUseCase;
+import com.alkemy.ong.application.service.usecase.IUpdateUserUseCase;
 import com.alkemy.ong.application.util.image.IImageUploader;
 import com.alkemy.ong.infrastructure.database.repository.CategoryRepository;
 import com.alkemy.ong.infrastructure.database.repository.SlideRepository;
@@ -154,6 +156,11 @@ public class SpringBeanConfiguration {
   public ICreateSlideUseCase createSlideUseCase(ISlideRepository slideRepository,
       IImageUploader imageUploader) {
     return new CreateSlideUseCaseService(slideRepository, imageUploader);
+  }
+
+  @Bean
+  public IUpdateUserUseCase updateUserUseCase(IUserRepository userRepository) {
+    return new UpdateUserUseCaseService(userRepository);
   }
 
 }
