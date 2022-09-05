c3e648e783a8592eed5998ecaca14dac1ccc965a
Ricardo Olivera <rholivera93@gmail.com>
2022-09-05 08:14:08 -0300

    OT275-35: added update method on user resource

--- src/main/java/com/alkemy/ong/infrastructure/rest/resource/UserResource.java
+++ src/main/java/com/alkemy/ong/infrastructure/rest/resource/UserResource.java
@@ -1,12 +1,20 @@
 package com.alkemy.ong.infrastructure.rest.resource;
 
 import com.alkemy.ong.application.service.usecase.IDeleteUserUseCase;
+import com.alkemy.ong.application.service.usecase.IUpdateUserUseCase;
+import com.alkemy.ong.domain.User;
+import com.alkemy.ong.infrastructure.rest.mapper.UserUpdateMapper;
+import com.alkemy.ong.infrastructure.rest.request.UserUpdateRequest;
+import com.alkemy.ong.infrastructure.rest.response.UserUpdateResponse;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,10 +24,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserResource {
 
   private final IDeleteUserUseCase deleteUserUseCase;
+  private final IUpdateUserUseCase updateUserUseCase;
+  private final UserUpdateMapper userUpdateMapper;
 
   @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<Void> delete(@PathVariable Long id) {
     deleteUserUseCase.delete(() -> id);
     return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+  }
+
+  @PutMapping(value = "/{id}",
+      produces = MediaType.APPLICATION_JSON_VALUE,
+      consumes = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<UserUpdateResponse> update(@PathVariable Long id,
+      @Valid @RequestBody UserUpdateRequest userUpdateRequest) {
+    User user = userUpdateMapper.toDomain(() -> id, userUpdateRequest);
+    User userUpdated = updateUserUseCase.update(user);
+    UserUpdateResponse response = userUpdateMapper.toResponse(userUpdated);
+    return ResponseEntity.ok(response);
   }
 }
