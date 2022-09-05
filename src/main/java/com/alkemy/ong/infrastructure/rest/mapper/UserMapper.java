66f794edba035019f9f4dd1ba86cea0f7ca83864
LAYALA09 <ayalilian2018@gmail.com>
2022-09-05 14:59:15 -0300

    OT275-36: Added UserMapper

---
+++ src/main/java/com/alkemy/ong/infrastructure/rest/mapper/UserMapper.java
@@ -0,0 +1,37 @@
+package com.alkemy.ong.infrastructure.rest.mapper;
+
+
+import com.alkemy.ong.domain.User;
+import com.alkemy.ong.infrastructure.rest.response.UserResponse;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class UserMapper {
+
+  public UserResponse toResponse(User user) {
+    if (user == null) {
+      return null;
+    }
+    UserResponse userResponse = new UserResponse();
+    userResponse.setFirstName(user.getFirstName());
+    userResponse.setLastName(user.getLastName());
+    userResponse.setEmail(user.getEmail());
+    userResponse.setRole(user.getRole().getName());
+    return userResponse;
+  }
+
+
+  public List<UserResponse> toResponse(List<User> userList) {
+    if (userList == null || userList.isEmpty()) {
+      return Collections.emptyList();
+    }
+    List<UserResponse> userResponses = new ArrayList<>(userList.size());
+    for (User user : userList) {
+      userResponses.add(toResponse(user));
+    }
+    return userResponses;
+
+  }
+
+}
