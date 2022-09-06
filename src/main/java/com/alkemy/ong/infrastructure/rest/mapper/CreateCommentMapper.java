4f2aed5e850d2d1271c7794e922019b2fd806ccc
Sieglind <moraledacd@gmail.com>
2022-09-06 20:39:45 -0300

    OT275-77: Created "CreateCommentMapper" class and methods

---
+++ src/main/java/com/alkemy/ong/infrastructure/rest/mapper/CreateCommentMapper.java
@@ -0,0 +1,31 @@
+package com.alkemy.ong.infrastructure.rest.mapper;
+
+import com.alkemy.ong.domain.Comment;
+import com.alkemy.ong.infrastructure.rest.request.CreateCommentRequest;
+import com.alkemy.ong.infrastructure.rest.response.CreateCommentResponse;
+
+public class CreateCommentMapper {
+
+  public Comment toDomain(CreateCommentRequest request) {
+    if (request == null){
+      return null;
+    }
+    Comment comment = new Comment();
+    comment.setBody(request.getBody());
+    return comment;
+  }
+
+  public CreateCommentResponse toResponse(Comment comment){
+    if (comment == null){
+      return null;
+    }
+    CreateCommentResponse response = new CreateCommentResponse();
+    response.setId(comment.getId());
+    response.setBody(comment.getBody());
+    response.setBody(comment.getBody());
+    response.setCreatedBy(comment.getCreatedBy().getFirstName());
+    response.setAssociatedNews(comment.getAssociatedNews().getName());
+    response.setCreateTimestamp(comment.getCreateTimestamp());
+    return response;
+  }
+}
