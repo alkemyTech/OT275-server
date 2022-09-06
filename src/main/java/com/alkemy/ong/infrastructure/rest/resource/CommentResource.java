af2329b1565a8803620108b643bed7a777a4e38b
Sieglind <moraledacd@gmail.com>
2022-09-06 20:21:52 -0300

    OT275-77: Created "CommentResource#create()" method

--- src/main/java/com/alkemy/ong/infrastructure/rest/resource/CommentResource.java
+++ src/main/java/com/alkemy/ong/infrastructure/rest/resource/CommentResource.java
@@ -2,8 +2,12 @@ package com.alkemy.ong.infrastructure.rest.resource;
 
 import com.alkemy.ong.application.service.usecase.IDeleteCommentUseCase;
 import com.alkemy.ong.application.service.usecase.IListCommentUseCase;
+import com.alkemy.ong.domain.Comment;
 import com.alkemy.ong.infrastructure.rest.mapper.ListCommentMapper;
+import com.alkemy.ong.infrastructure.rest.request.CreateCommentRequest;
+import com.alkemy.ong.infrastructure.rest.response.CreateCommentResponse;
 import com.alkemy.ong.infrastructure.rest.response.ListCommentResponse;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -11,6 +15,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +27,9 @@ public class CommentResource {
 
   private final IDeleteCommentUseCase deleteCommentUseCase;
   private final IListCommentUseCase listCommentUseCase;
+  private final ICreateCommentUseCase createCommentUseCase;
   private final ListCommentMapper listCommentMapper;
+  private final CreateCommentMapper createCommentMapper;
 
   @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<Void> delete(@PathVariable Long id) {
@@ -33,5 +41,17 @@ public class CommentResource {
   public ResponseEntity<ListCommentResponse> list() {
     return ResponseEntity.ok().body(listCommentMapper
         .toResponse(listCommentUseCase.findAllOrderedByTimestamp()));
+  }
+
+  @PostMapping(
+      consumes = MediaType.APPLICATION_JSON_VALUE,
+      produces = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<CreateCommentResponse> create(
+      @Valid @RequestBody CreateCommentRequest createCommentRequest) {
+    Comment savedComment = createCommentUseCase.create(
+        createCommentMapper.toDomain(createCommentRequest),
+        () -> createCommentRequest.getUserId(),
+        () -> createCommentRequest.getNewsId());
+    return createCommentMapper.toResponse(savedComment);
   }
 }
