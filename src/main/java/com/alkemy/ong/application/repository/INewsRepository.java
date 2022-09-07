ac6150638f0c2377e408489f32d59dc28df54e0e
Sieglind <moraledacd@gmail.com>
2022-09-06 21:51:58 -0300

    OT275-77: Created "INewsRepository"

--- src/main/java/com/alkemy/ong/application/repository/INewsRepository.java
+++ src/main/java/com/alkemy/ong/application/repository/INewsRepository.java
@@ -1,10 +1,14 @@
 package com.alkemy.ong.application.repository;
 
 import com.alkemy.ong.domain.Identifiable;
+import com.alkemy.ong.domain.News;
+import com.alkemy.ong.domain.User;
 
 public interface INewsRepository {
 
   void delete(Identifiable<Long> identifiable);
 
   boolean exists(Identifiable<Long> identifiable);
+
+  News findBy(Identifiable<Long> identifiable);
 }
