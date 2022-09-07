771edd50b6f522a893d480f5b396ede68be79333
Sieglind <moraledacd@gmail.com>
2022-09-07 00:02:20 -0300

    OT275-77: Reused existing validation

--- src/main/java/com/alkemy/ong/infrastructure/rest/request/CreateCommentRequest.java
+++ src/main/java/com/alkemy/ong/infrastructure/rest/request/CreateCommentRequest.java
@@ -1,6 +1,6 @@
 package com.alkemy.ong.infrastructure.rest.request;
 
-import com.alkemy.ong.infrastructure.rest.request.validation.NotBlankAlphanumeric;
+import com.alkemy.ong.infrastructure.rest.request.validation.CharactersWithWhiteSpaces;
 import javax.validation.constraints.NotNull;
 import lombok.Getter;
 
@@ -12,6 +12,6 @@ public class CreateCommentRequest {
   @NotNull(message = "User Id must not be null")
   private Long newsId;
   @NotNull(message = "Comment body mut not be null")
-  @NotBlankAlphanumeric(message = "Comment body must only contain letters and numbers, and not be empty")
+  @CharactersWithWhiteSpaces(message = "Comment body must only contain letters and numbers, and not be empty")
   private String body;
 }
