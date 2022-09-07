07fc88302c9cf0503db87e19feabd9e7a4a88349
René Guardia <reneguardia@outlook.com>
2022-09-07 13:44:28 -0300

    OT275-62: Implements findAll in repositories

--- src/main/java/com/alkemy/ong/application/repository/IContactRepository.java
+++ src/main/java/com/alkemy/ong/application/repository/IContactRepository.java
@@ -1,5 +1,10 @@
 package com.alkemy.ong.application.repository;
 
+import com.alkemy.ong.domain.Contact;
+import java.util.List;
+
 public interface IContactRepository {
+
+  List<Contact> findAll();
 
 }
