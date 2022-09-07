b205a27524234a5ad4dc9423e5322f753ba22237
Sieglind <moraledacd@gmail.com>
2022-09-07 18:29:02 -0300

    OT275-77: Fixed AntMatchers and documentation paths

--- src/main/java/com/alkemy/ong/infrastructure/config/spring/security/SecurityConfig.java
+++ src/main/java/com/alkemy/ong/infrastructure/config/spring/security/SecurityConfig.java
@@ -42,7 +42,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   private static final String USERS_ID_URL = "/users/{id:[\\d+]}";
   private static final String MEMBERS_ID_URL = "/members/{id:[\\d+]}";
   private static final String TESTIMONIALS_ID_URL = "/testimonials/{id:[\\d+]}";
-  private static final String DOCUMENTATION_PATH = "/api/**";
+  private static final String[] DOCUMENTATION_PATHS = {"/api/docs",
+      "/api/swagger-ui/**",
+      "/api/docs/oas/swagger-config",
+      "/api/docs/oas/",
+      "/api/docs/oas.yaml"};
+
   private static final String DOCUMENTATION_URL = "/documentation.yaml";
   private static final String USERS_URL = "/users";
 
@@ -86,7 +91,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         .and()
         .authorizeRequests()
-        .antMatchers(DOCUMENTATION_PATH)
+        .antMatchers(DOCUMENTATION_PATHS)
         .permitAll()
         .antMatchers(DOCUMENTATION_URL)
         .permitAll()
