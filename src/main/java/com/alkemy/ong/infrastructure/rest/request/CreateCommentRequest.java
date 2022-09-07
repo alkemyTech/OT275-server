689f8d8523abad0485bcaf95f4ca023a47f3021d
Sieglind <moraledacd@gmail.com>
2022-09-07 00:06:33 -0300

    OT275-77: Fixed style

--- src/main/java/com/alkemy/ong/infrastructure/rest/request/CreateCommentRequest.java
+++ src/main/java/com/alkemy/ong/infrastructure/rest/request/CreateCommentRequest.java
@@ -12,6 +12,7 @@ public class CreateCommentRequest {
   @NotNull(message = "User Id must not be null")
   private Long newsId;
   @NotNull(message = "Comment body mut not be null")
-  @CharactersWithWhiteSpaces(message = "Comment body must only contain letters and numbers, and not be empty")
+  @CharactersWithWhiteSpaces(
+      message = "Comment body must only contain letters and numbers, and not be empty")
   private String body;
 }
