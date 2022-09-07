c0805873356d58d41f6c9dbabb8226e35ebd1583
René Guardia <reneguardia@outlook.com>
2022-09-07 13:46:02 -0300

    OT275-62: Added Contact domain object

---
+++ src/main/java/com/alkemy/ong/domain/Contact.java
@@ -0,0 +1,14 @@
+package com.alkemy.ong.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Contact {
+
+  private String name;
+  private String phone;
+  private String email;
+
+}
