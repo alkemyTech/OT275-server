23fd5f59ee7c3f3bc34a8ef3c8d03da7bc9191e7
Sieglind <moraledacd@gmail.com>
2022-09-07 15:34:40 -0300

    OT275-77: Fixed validation

--- src/main/java/com/alkemy/ong/infrastructure/rest/request/CreateCommentRequest.java
+++ src/main/java/com/alkemy/ong/infrastructure/rest/request/CreateCommentRequest.java
@@ -1,6 +1,6 @@
 package com.alkemy.ong.infrastructure.rest.request;
 
-import com.alkemy.ong.infrastructure.rest.request.validation.CharactersWithWhiteSpaces;
+import com.alkemy.ong.infrastructure.rest.request.validation.NotBlankAlphanumeric;
 import javax.validation.constraints.NotNull;
 import lombok.Getter;
 
@@ -12,7 +12,7 @@ public class CreateCommentRequest {
   @NotNull(message = "User Id must not be null")
   private Long newsId;
   @NotNull(message = "Comment body mut not be null")
-  @CharactersWithWhiteSpaces(
-      message = "Comment body must only contain letters and numbers, and not be empty")
+  @NotBlankAlphanumeric(
+      message = "Comment body content must be alphanumeric")
   private String body;
 }
