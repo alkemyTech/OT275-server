72efd5c31791746a593ebe71f649688855d5bb28
René Guardia <reneguardia@outlook.com>
2022-09-07 19:00:25 -0300

    OT275-47: Added CreateNewsUseCaseServiceTest

---
+++ src/test/java/com/alkemy/ong/application/service/CreateNewsUseCaseServiceTest.java
@@ -0,0 +1,39 @@
+package com.alkemy.ong.application.service;
+
+import static org.mockito.Mockito.verify;
+
+import com.alkemy.ong.application.repository.INewsRepository;
+import com.alkemy.ong.common.NewsBuilder;
+import com.alkemy.ong.domain.News;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class CreateNewsUseCaseServiceTest {
+
+  private CreateNewsUseCaseService createNewsUseCaseService;
+
+  @Mock
+  private INewsRepository newsRepository;
+
+  @Mock
+  private News news;
+
+  @BeforeEach
+  void setUp() {
+    createNewsUseCaseService = new CreateNewsUseCaseService(newsRepository);
+  }
+
+  @Test
+  void shouldAddNews() {
+    news = NewsBuilder.random();
+
+    createNewsUseCaseService.add(news);
+
+    verify(newsRepository).add(news);
+  }
+
+}
