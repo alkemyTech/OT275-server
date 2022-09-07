daeb92bc65fd5469f3d1c0379628bd75b25b5cf1
René Guardia <reneguardia@outlook.com>
2022-09-07 18:54:58 -0300

    OT275-47: Implements add in News resource

--- src/main/java/com/alkemy/ong/infrastructure/rest/resource/NewsResource.java
+++ src/main/java/com/alkemy/ong/infrastructure/rest/resource/NewsResource.java
@@ -1,9 +1,15 @@
 package com.alkemy.ong.infrastructure.rest.resource;
 
+import com.alkemy.ong.application.service.usecase.ICreateNewsUseCase;
 import com.alkemy.ong.application.service.usecase.IDeleteNewsUseCase;
 import com.alkemy.ong.application.service.usecase.IGetNewsUseCase;
+import com.alkemy.ong.domain.News;
+import com.alkemy.ong.infrastructure.rest.mapper.CreateNewsMapper;
 import com.alkemy.ong.infrastructure.rest.mapper.GetNewsMapper;
+import com.alkemy.ong.infrastructure.rest.request.CreateNewsRequest;
 import com.alkemy.ong.infrastructure.rest.response.GetNewsResponse;
+import com.alkemy.ong.infrastructure.rest.response.NewsResponse;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -11,6 +17,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,10 +28,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class NewsResource {
 
   private final IDeleteNewsUseCase deleteNewsUseCase;
-
   private final IGetNewsUseCase getNewsUseCase;
-
   private final GetNewsMapper getNewsMapper;
+  private final ICreateNewsUseCase createNewsUseCase;
+  private final CreateNewsMapper createNewsMapper;
 
   @DeleteMapping(value = "{id}", produces = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<Void> delete(@PathVariable Long id) {
@@ -34,6 +42,15 @@ public class NewsResource {
   @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<GetNewsResponse> get(@PathVariable Long id) {
     return ResponseEntity.ok(getNewsMapper.toResponse(getNewsUseCase.get(() -> id)));
+  }
+
+  @PostMapping(
+      produces = MediaType.APPLICATION_JSON_VALUE,
+      consumes = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<NewsResponse> add(@Valid @RequestBody CreateNewsRequest newsRequest) {
+    News news = createNewsMapper.toDomain(newsRequest);
+    News createdNews = createNewsUseCase.add(news);
+    return new ResponseEntity<>(createNewsMapper.toResponse(createdNews), HttpStatus.CREATED);
   }
 
 }
