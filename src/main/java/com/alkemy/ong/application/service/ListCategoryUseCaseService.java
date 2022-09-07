6d9ad874c0b637feb431b80625de0049deda6ee7
Ricardo Olivera <rholivera93@gmail.com>
2022-09-07 04:41:34 -0300

    OT275-81: modify return type of findAll method

--- src/main/java/com/alkemy/ong/application/service/ListCategoryUseCaseService.java
+++ src/main/java/com/alkemy/ong/application/service/ListCategoryUseCaseService.java
@@ -3,8 +3,9 @@ package com.alkemy.ong.application.service;
 import com.alkemy.ong.application.repository.ICategoryRepository;
 import com.alkemy.ong.application.service.usecase.IListCategoryUseCase;
 import com.alkemy.ong.domain.Category;
-import java.util.List;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 @AllArgsConstructor
 public class ListCategoryUseCaseService implements IListCategoryUseCase {
@@ -12,7 +13,7 @@ public class ListCategoryUseCaseService implements IListCategoryUseCase {
   private final ICategoryRepository categoryRepository;
 
   @Override
-  public List<Category> findAll() {
-    return categoryRepository.findAll();
+  public Page<Category> findAll(Pageable pageable) {
+    return categoryRepository.findAll(pageable);
   }
 }
