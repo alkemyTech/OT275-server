47fa53d3da93412db5db5df6682ccbb555c02fe2
FedericoRohr <rohrfede@gmail.com>
2022-09-08 18:12:13 -0300

    OT275-66: added save method

--- src/main/java/com/alkemy/ong/application/repository/ITestimonialRepository.java
+++ src/main/java/com/alkemy/ong/application/repository/ITestimonialRepository.java
@@ -1,6 +1,7 @@
 package com.alkemy.ong.application.repository;
 
 import com.alkemy.ong.domain.Identifiable;
+import com.alkemy.ong.domain.Testimonial;
 
 public interface ITestimonialRepository {
 
@@ -8,4 +9,5 @@ public interface ITestimonialRepository {
 
   boolean exists(Identifiable<Long> identifiable);
 
+  Testimonial save(Testimonial testimonial);
 }
