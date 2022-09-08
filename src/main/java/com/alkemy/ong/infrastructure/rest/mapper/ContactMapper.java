77004b651dbca46d383c5c429ecd120a808aff17
Sieglind <moraledacd@gmail.com>
2022-09-08 18:15:54 -0300

    OT275-6: Created Contact Mapper and Resource

---
+++ src/main/java/com/alkemy/ong/infrastructure/rest/mapper/ContactMapper.java
@@ -0,0 +1,35 @@
+package com.alkemy.ong.infrastructure.rest.mapper;
+
+import com.alkemy.ong.domain.Contact;
+import com.alkemy.ong.infrastructure.rest.request.ContactRequest;
+import com.alkemy.ong.infrastructure.rest.response.ContactResponse;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ContactMapper {
+
+  public Contact toDomain(ContactRequest request) {
+    if (request == null) {
+      return null;
+    }
+    Contact contact = new Contact();
+    contact.setName(request.getName());
+    contact.setPhone(request.getPhone());
+    contact.setEmail(request.getEmail());
+    contact.setMessage(request.getMessage());
+    return contact;
+  }
+
+  public ContactResponse toResponse(Contact contact) {
+    if (contact == null) {
+      return null;
+    }
+    ContactResponse response = new ContactResponse();
+    response.setId(contact.getId());
+    response.setName(contact.getName());
+    response.setPhone(contact.getPhone());
+    response.setEmail(contact.getEmail());
+    response.setMessage(contact.getMessage());
+    return response;
+  }
+}
--- src/main/java/com/alkemy/ong/infrastructure/rest/resource/ContactResource.java
+++ src/main/java/com/alkemy/ong/infrastructure/rest/resource/ContactResource.java
@@ -1,12 +1,21 @@
 package com.alkemy.ong.infrastructure.rest.resource;
 
+import com.alkemy.ong.application.service.usecase.ICreateContactUseCase;
 import com.alkemy.ong.application.service.usecase.IListContactUseCase;
+import com.alkemy.ong.domain.Contact;
+import com.alkemy.ong.infrastructure.rest.mapper.ContactMapper;
 import com.alkemy.ong.infrastructure.rest.mapper.ListContactMapper;
+import com.alkemy.ong.infrastructure.rest.request.ContactRequest;
+import com.alkemy.ong.infrastructure.rest.response.ContactResponse;
 import com.alkemy.ong.infrastructure.rest.response.ListContactResponse;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +25,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class ContactResource {
 
   private final IListContactUseCase listContactUseCase;
+  private final ICreateContactUseCase createContactUseCase;
+  private final ContactMapper contactMapper;
   private final ListContactMapper listContactMapper;
+
+  @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+  public ResponseEntity<ContactResponse> create(@Valid @RequestBody ContactRequest contactRequest) {
+    Contact savedContact = createContactUseCase.create(contactMapper.toDomain(contactRequest));
+    return new ResponseEntity<>(contactMapper.toResponse(savedContact), HttpStatus.CREATED);
+  }
 
   @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
   public ResponseEntity<ListContactResponse> list() {
