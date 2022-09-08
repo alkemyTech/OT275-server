d3b3f93eb8d5f1ac9b785d4d9452eb17b069e6a5
Sieglind <moraledacd@gmail.com>
2022-09-08 18:17:02 -0300

    OT275-6: Created usecase service and implementation, added method to repository interface, modified Contact domain class

--- src/main/java/com/alkemy/ong/application/repository/IContactRepository.java
+++ src/main/java/com/alkemy/ong/application/repository/IContactRepository.java
@@ -2,9 +2,14 @@ package com.alkemy.ong.application.repository;
 
 import com.alkemy.ong.domain.Contact;
 import java.util.List;
+import java.util.Optional;
 
 public interface IContactRepository {
 
   List<Contact> findAll();
+
+  Optional<Object> findBy(String name);
+
+  Contact create(Contact contact);
 
 }
---
+++ src/main/java/com/alkemy/ong/application/service/usecase/ICreateContactUseCase.java
@@ -0,0 +1,8 @@
+package com.alkemy.ong.application.service.usecase;
+
+import com.alkemy.ong.domain.Contact;
+
+public interface ICreateContactUseCase {
+
+  Contact create(Contact contact);
+}
