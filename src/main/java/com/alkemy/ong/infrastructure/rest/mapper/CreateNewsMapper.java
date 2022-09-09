2879da562b4cc9271f155f10ceebdbe8ef57c831
René Guardia <reneguardia@outlook.com>
2022-09-09 14:08:06 -0300

    OT275-47: CreateNewsMapper extends GetNewsMapper

--- src/main/java/com/alkemy/ong/infrastructure/rest/mapper/CreateNewsMapper.java
+++ src/main/java/com/alkemy/ong/infrastructure/rest/mapper/CreateNewsMapper.java
@@ -2,11 +2,14 @@ package com.alkemy.ong.infrastructure.rest.mapper;
 
 import com.alkemy.ong.domain.News;
 import com.alkemy.ong.infrastructure.rest.request.CreateNewsRequest;
-import com.alkemy.ong.infrastructure.rest.response.NewsResponse;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CreateNewsMapper {
+public class CreateNewsMapper extends GetNewsMapper {
+
+  public CreateNewsMapper(GetCategoryMapper getCategoryMapper) {
+    super(getCategoryMapper);
+  }
 
   public News toDomain(CreateNewsRequest newsRequest) {
     if (newsRequest == null) {
@@ -15,20 +18,8 @@ public class CreateNewsMapper {
     News news = new News();
     news.setName(newsRequest.getName());
     news.setContent(newsRequest.getText());
-    news.setImageUrl(newsRequest.getImageUrl());
+    news.setImageUrl(newsRequest.getImage());
     return news;
-  }
-
-  public NewsResponse toResponse(News news) {
-    if (news == null) {
-      return null;
-    }
-    NewsResponse newsResponse = new NewsResponse();
-    newsResponse.setId(news.getId());
-    newsResponse.setName(news.getName());
-    newsResponse.setText(news.getContent());
-    newsResponse.setImageUrl(news.getImageUrl());
-    return newsResponse;
   }
 
 }
