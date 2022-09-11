c26f6f4b27c3a0443eefc7741bc7cee4e0687e2b
FedericoRohr <rohrfede@gmail.com>
2022-09-11 20:07:36 -0300

    OT275-66: Fixed CreateTestimonialRequest class

--- src/main/java/com/alkemy/ong/infrastructure/rest/request/CreateTestimonialRequest.java
+++ src/main/java/com/alkemy/ong/infrastructure/rest/request/CreateTestimonialRequest.java
@@ -1,8 +1,8 @@
 package com.alkemy.ong.infrastructure.rest.request;
 
+import com.alkemy.ong.infrastructure.rest.request.validation.AlphanumericWithWhiteSpaces;
 import com.alkemy.ong.infrastructure.rest.request.validation.CharactersWithWhiteSpaces;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -15,15 +15,14 @@ public class CreateTestimonialRequest {
 
 
   @NotEmpty(message = "name must not be empty.")
-  @NotNull(message = "name must not be null.")
   @CharactersWithWhiteSpaces(message = "Name must contain only spaces and letters.")
   @Size(message = "Name must shorter than 50 characters.", max = 50)
   private String name;
 
-
+  @AlphanumericWithWhiteSpaces
   @Size(message = "Content must shorter than 150 characters.", max = 150)
   private String content;
 
 
-  private String imageUrl;
+  private String image;
 }
