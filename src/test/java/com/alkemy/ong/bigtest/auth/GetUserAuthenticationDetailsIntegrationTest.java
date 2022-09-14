501f0bac69317c578c90982017960c113642f419
Ricardo Olivera <rholivera93@gmail.com>
2022-09-14 17:00:06 -0300

    OT275-97: Added integration test of get user authentication details

---
+++ src/test/java/com/alkemy/ong/bigtest/auth/GetUserAuthenticationDetailsIntegrationTest.java
@@ -0,0 +1,28 @@
+package com.alkemy.ong.bigtest.auth;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.alkemy.ong.bigtest.BigTest;
+import org.junit.Test;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+
+public class GetUserAuthenticationDetailsIntegrationTest extends BigTest {
+
+  private static final String URL = "/auth/me";
+
+  @Test
+  public void shouldReturnUserInformationWhenRequestHasValidToken() throws Exception {
+    mockMvc.perform(get(URL)
+            .contentType(MediaType.APPLICATION_JSON)
+            .header(HttpHeaders.AUTHORIZATION, getAuthorizationTokenForStandardUser()))
+        .andExpect(jsonPath("$.email", equalTo("freedy@krueger.com")))
+        .andExpect(jsonPath("$.firstName", equalTo("Freddy")))
+        .andExpect(jsonPath("$.lastName", equalTo("Krueger")))
+        .andExpect(jsonPath("$.imageUrl", equalTo(null)))
+        .andExpect(status().isOk());
+  }
+}
