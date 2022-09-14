61179e5731952d33c37f2a5bd5991822547f3398
René Guardia <reneguardia@outlook.com>
2022-09-13 21:15:07 -0300

    OT275-95: Fixed imports and removed duplicate code.

--- src/test/java/com/alkemy/ong/builder/UpdateActivityRequestBuilder.java
+++ src/test/java/com/alkemy/ong/builder/UpdateActivityRequestBuilder.java
@@ -1,19 +1,18 @@
 package com.alkemy.ong.builder;
 
-import com.alkemy.ong.infrastructure.rest.request.UpdateActivityRequest;
+import com.alkemy.ong.infrastructure.rest.request.activity.UpdateActivityRequest;
 
 public class UpdateActivityRequestBuilder {
 
   public static UpdateActivityRequest defaultRequest() {
-    UpdateActivityRequest updateActivityRequest = new UpdateActivityRequest();
-    updateActivityRequest.setName("My Activity");
-    updateActivityRequest.setContent("My Activity content");
-    updateActivityRequest.setImageUrl("https://s3.com/default-image.jpg");
-    return updateActivityRequest;
+    return buildRequest(
+        "Updated Activity",
+        "Updated Activity content",
+        "https://s3.com/default-image.jpg");
   }
 
   public static UpdateActivityRequest buildRequest(String name, String content, String imageUrl) {
-    UpdateActivityRequest updateActivityRequest = defaultRequest();
+    UpdateActivityRequest updateActivityRequest = new UpdateActivityRequest();
     updateActivityRequest.setName(name);
     updateActivityRequest.setContent(content);
     updateActivityRequest.setImageUrl(imageUrl);
