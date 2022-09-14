4c0f1016a37652b06db1afbfe2ff92f0ab0f58c5
Magali Kain <magali.kain@thoughtworks.com>
2022-09-14 11:22:56 +0200

    Add incomplete integration test to avoid commiting changes that must be done by the author

--- src/main/java/com/alkemy/ong/infrastructure/rest/request/comment/UpdateCommentRequest.java
+++ src/main/java/com/alkemy/ong/infrastructure/rest/request/comment/UpdateCommentRequest.java
@@ -1,8 +1,10 @@
 package com.alkemy.ong.infrastructure.rest.request.comment;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class UpdateCommentRequest {
   private String body;
 }
