c87f5e965d570070cf5385b6a7eeda88d750c165
FedericoRohr <rohrfede@gmail.com>
2022-09-16 17:43:39 -0300

    OT275-96: Added shouldReturnForbiddenWhenUserHasStandardRole method

---
+++ src/test/java/com/alkemy/ong/bigtest/contact/ListContactIntegration.java
@@ -0,0 +1,29 @@
+package com.alkemy.ong.bigtest.contact;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.alkemy.ong.bigtest.BigTest;
+import org.junit.Test;
+import org.springframework.http.HttpHeaders;
+
+public class ListContactIntegration extends BigTest {
+
+  private static final String URL = "/contacts";
+
+  @Test
+  public void shouldReturnForbiddenWhenUserHasStandardRole() throws Exception {
+    mockMvc.perform(get(URL)
+            .header(HttpHeaders.AUTHORIZATION, getAuthorizationTokenForStandardUser()))
+        .andExpect(jsonPath("$.statusCode", equalTo(403)))
+        .andExpect(jsonPath("$.message", equalTo(ACCESS_DENIED_MESSAGE)))
+        .andExpect(jsonPath("$.moreInfo", hasSize(1)))
+        .andExpect(jsonPath("$.moreInfo", hasItem(ACCESS_DENIED_MORE_INFO)))
+        .andExpect(status().isForbidden());
+  }
+
+}
