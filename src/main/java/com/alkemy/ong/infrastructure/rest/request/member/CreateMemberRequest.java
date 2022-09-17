7362119442677a4a531544be82da213ebf73c698
René Guardia <reneguardia@outlook.com>
2022-09-16 21:35:46 -0300

    OT275-70: Added CreateMemberRequest.

---
+++ src/main/java/com/alkemy/ong/infrastructure/rest/request/member/CreateMemberRequest.java
@@ -0,0 +1,29 @@
+package com.alkemy.ong.infrastructure.rest.request.member;
+
+import com.alkemy.ong.infrastructure.rest.request.validation.AlphanumericWithoutWhiteSpaces;
+import com.alkemy.ong.infrastructure.rest.request.validation.CharactersWithWhiteSpaces;
+import javax.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateMemberRequest {
+
+  @NotEmpty(message = "Name cannot be empty.")
+  @CharactersWithWhiteSpaces(message = "Name must contain only spaces and letters.")
+  private String name;
+
+  @NotEmpty(message = "Image cannot be empty.")
+  @AlphanumericWithoutWhiteSpaces(message = "Image must be alphanumeric without white spaces.")
+  private String image;
+
+  private String description;
+
+  private String facebookUrl;
+
+  private String instagramUrl;
+
+  private String linkedInUrl;
+
+}
