1c2e9af38f62303cad217bed109a37ec706ceb93
FedericoRohr <104379574+FedericoRohr@users.noreply.github.com>
2022-09-19 16:16:43 -0300

    OT275-96: Added test of endpoints /contact (#91)

--- src/main/java/com/alkemy/ong/infrastructure/rest/request/contact/CreateContactRequest.java
+++ src/main/java/com/alkemy/ong/infrastructure/rest/request/contact/CreateContactRequest.java
@@ -3,9 +3,11 @@ package com.alkemy.ong.infrastructure.rest.request.contact;
 import com.alkemy.ong.infrastructure.rest.request.validation.CharactersWithWhiteSpaces;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class CreateContactRequest {
 
   @CharactersWithWhiteSpaces(message = "Name must contain only spaces and letters.")
