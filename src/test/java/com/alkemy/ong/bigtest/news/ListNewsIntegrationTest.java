84a2d028a4eff6fed7bac56f859637d86cfb3049
Sieglind <moraledacd@gmail.com>
2022-09-19 18:22:28 -0300

    OT275-72: asd

---
+++ src/test/java/com/alkemy/ong/bigtest/news/ListNewsIntegrationTest.java
@@ -0,0 +1,9 @@
+package com.alkemy.ong.bigtest.news;
+
+import com.alkemy.ong.bigtest.BigTest;
+
+public class ListNewsIntegrationTest extends BigTest {
+
+  private static final String URL = "/news";
+
+}
