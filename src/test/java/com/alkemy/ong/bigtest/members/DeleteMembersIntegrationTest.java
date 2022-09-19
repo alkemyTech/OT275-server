55ebca5bc68eddddab94551955c186db2b7400a5
FedericoRohr <rohrfede@gmail.com>
2022-09-19 20:48:49 -0300

    OT275-92: Added assertMemberHasBeenDeleted method

--- src/test/java/com/alkemy/ong/bigtest/members/DeleteMembersIntegrationTest.java
+++ src/test/java/com/alkemy/ong/bigtest/members/DeleteMembersIntegrationTest.java
@@ -1,7 +1,18 @@
 package com.alkemy.ong.bigtest.members;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import com.alkemy.ong.bigtest.BigTest;
+
+import com.alkemy.ong.infrastructure.database.entity.MemberEntity;
+import java.util.Optional;
 
 public class DeleteMembersIntegrationTest extends BigTest {
 
+
+  private void assertMemberHasBeenDeleted(Long randomMemberId) {
+    Optional<MemberEntity> member = memberRepository.findById(randomMemberId);
+    assertTrue(member.isPresent());
+    assertTrue(member.get().isSoftDeleted());
+  }
 }
