64899f5367b20b38f0e1b41744fc92a4473d128e
Sieglind <moraledacd@gmail.com>
2022-09-19 17:23:05 -0300

    OT275-72: Created UpdateMemberUseCaseService and added IMemberRepository#update method

---
+++ src/main/java/com/alkemy/ong/application/service/member/UpdateMemberUseCaseService.java
@@ -0,0 +1,21 @@
+package com.alkemy.ong.application.service.member;
+
+import com.alkemy.ong.application.exception.ErrorMessage;
+import com.alkemy.ong.application.exception.ObjectNotFoundException;
+import com.alkemy.ong.application.repository.IMemberRepository;
+import com.alkemy.ong.application.service.member.usecase.IUpdateMemberUseCase;
+import com.alkemy.ong.domain.Member;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class UpdateMemberUseCaseService implements IUpdateMemberUseCase {
+  private final IMemberRepository memberRepository;
+
+  @Override
+  public Member update(Member member) {
+    if (!memberRepository.exists(member::getMemberId)){
+      throw new ObjectNotFoundException(ErrorMessage.OBJECT_NOT_FOUND.getMessage("Member"));
+    }
+    return memberRepository.update(member);
+  }
+}
