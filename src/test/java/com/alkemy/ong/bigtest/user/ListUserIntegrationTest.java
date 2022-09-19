77c3b8e5c05b64cc9335e7507d967ca98e363e2c
LAYALA09 <ayalilian2018@gmail.com>
2022-09-19 10:42:05 -0300

    OT275-91: I add changes ListUserIntegrationTest

--- src/test/java/com/alkemy/ong/bigtest/user/ListUserIntegrationTest.java
+++ src/test/java/com/alkemy/ong/bigtest/user/ListUserIntegrationTest.java
@@ -6,30 +6,29 @@ import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import com.alkemy.ong.bigtest.BigTest;
+import com.alkemy.ong.infrastructure.database.entity.UserEntity;
 import org.junit.Test;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 
 
 public class ListUserIntegrationTest extends BigTest {
 
-  private static final String URL = "/users";
-
   @Test
   public void shouldListUsersWhenUserHasAdminRole() throws Exception {
-
-    mockMvc.perform(get(URL)
+    UserEntity randomUser = getRandomUser();
+    mockMvc.perform(get("/users")
+        .contentType(MediaType.APPLICATION_JSON)
         .header(HttpHeaders.AUTHORIZATION, getAuthorizationTokenForAdminUser()))
-
-        .andExpect(jsonPath("$.users", hasSize(12)))
-
+        .andExpect(jsonPath("$.users", hasSize(13)))
         .andExpect(status().isOk());
+    cleanUsersData(randomUser);
   }
 
   @Test
   public void shouldReturnForbiddenWhenUserHasStandardRole() throws Exception {
-    mockMvc.perform(get(URL)
+    mockMvc.perform(get("/users")
         .header(HttpHeaders.AUTHORIZATION, getAuthorizationTokenForStandardUser()))
         .andExpect(jsonPath("$.statusCode", equalTo(403)))
         .andExpect(jsonPath("$.message", equalTo(ACCESS_DENIED_MESSAGE)))
