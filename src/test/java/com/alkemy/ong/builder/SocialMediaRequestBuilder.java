29b82f957c57e1dbc7d37be80bb0dd74bf3a0d00
FedericoRohr <rohrfede@gmail.com>
2022-09-20 13:03:38 -0300

    OT275-92:Fixed duplicate code

--- src/test/java/com/alkemy/ong/builder/SocialMediaRequestBuilder.java
+++ src/test/java/com/alkemy/ong/builder/SocialMediaRequestBuilder.java
@@ -5,11 +5,9 @@ import com.alkemy.ong.infrastructure.rest.request.common.SocialMediaRequest;
 public class SocialMediaRequestBuilder {
 
   public static SocialMediaRequest defaultSocialMediaRequest() {
-    SocialMediaRequest socialMediaRequest = new SocialMediaRequest();
-    socialMediaRequest.setFacebookUrl("https://www.facebook.com/somos_mas/");
-    socialMediaRequest.setLinkedInUrl("https://www.linkedin.com/in/somos_mas/");
-    socialMediaRequest.setInstagramUrl("https://www.instagram.com/SOMOSMAS/");
-    return socialMediaRequest;
+    return buildRequest("https://www.facebook.com/somos_mas/",
+        "https://www.linkedin.com/in/somos_mas/",
+        "https://www.instagram.com/SOMOSMAS/");
   }
 
   public static SocialMediaRequest buildRequest(String facebookUrl,String linkedinUrl
