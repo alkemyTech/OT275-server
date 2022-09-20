f3be787611d1aa82962b5c832df530a3202f9e5b
FedericoRohr <rohrfede@gmail.com>
2022-09-19 21:25:18 -0300

    OT275-92: Added shouldReturnNotFoundWhenMemberDoesNotExist  method

--- src/test/java/com/alkemy/ong/bigtest/members/DeleteMembersIntegrationTest.java
+++ src/test/java/com/alkemy/ong/bigtest/members/DeleteMembersIntegrationTest.java
@@ -1,13 +1,33 @@
 package com.alkemy.ong.bigtest.members;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.alkemy.ong.bigtest.BigTest;
 
 import com.alkemy.ong.infrastructure.database.entity.MemberEntity;
 import java.util.Optional;
+import org.junit.Test;
+import org.springframework.http.HttpHeaders;
 
 public class DeleteMembersIntegrationTest extends BigTest {
+  public static String URL = "/members/{id}/";
+
+  @Test
+  public void shouldReturnNotFoundWhenMemberDoesNotExist()throws Exception{
+    mockMvc.perform(delete(URL, "-1")
+            .header(HttpHeaders.AUTHORIZATION, getAuthorizationTokenForAdminUser()))
+        .andExpect(jsonPath("$.statusCode", equalTo(404)))
+        .andExpect(jsonPath("$.message", equalTo(OBJECT_NOT_FOUND_MESSAGE)))
+        .andExpect(jsonPath("$.moreInfo", hasSize(1)))
+        .andExpect(jsonPath("$.moreInfo", hasItem("Member not found.")))
+        .andExpect(status().isNotFound());
+  }
 
 
   private void assertMemberHasBeenDeleted(Long randomMemberId) {
